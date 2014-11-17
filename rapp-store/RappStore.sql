6c010382b1abbb1cae1674ea195ffe8da931b0c8
Alex Ge <alexge233@hotmail.com>
2014-11-17 04:41:01 +0000

    updated mysqldb

--- rapp-store/RappStore.sql
+++ rapp-store/RappStore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 15, 2014 at 06:35 AM
+-- Generation Time: Nov 17, 2014 at 04:39 AM
 -- Server version: 5.5.40
 -- PHP Version: 5.4.4-14+deb7u14
 
@@ -93,10 +93,10 @@ CREATE TABLE IF NOT EXISTS `tblRobot` (
 
 --
 -- RELATIONS FOR TABLE `tblRobot`:
---   `model`
---       `tblModel` -> `id`
 --   `owner`
 --       `tblUser` -> `id`
+--   `model`
+--       `tblModel` -> `id`
 --
 
 -- --------------------------------------------------------
@@ -104,23 +104,34 @@ CREATE TABLE IF NOT EXISTS `tblRobot` (
 --
 -- Table structure for table `tblUser`
 --
--- Creation: Nov 14, 2014 at 07:21 PM
+-- Creation: Nov 17, 2014 at 03:17 AM
 --
 
 DROP TABLE IF EXISTS `tblUser`;
 CREATE TABLE IF NOT EXISTS `tblUser` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
   `firstname` varchar(128) NOT NULL,
   `lastname` varchar(128) NOT NULL,
   `email` varchar(254) NOT NULL,
   `pwd` char(64) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `usrgroup` varchar(5) NOT NULL DEFAULT 'guest',
+  `usrgroup` int(1) NOT NULL DEFAULT '5',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `accessed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `activation` binary(16) NOT NULL COMMENT 'uuid-v4',
   UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `uid` (`firstname`,`lastname`,`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `uid` (`firstname`,`lastname`,`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `tblUser`
+--
+
+INSERT INTO `tblUser` (`id`, `username`, `firstname`, `lastname`, `email`, `pwd`, `usrgroup`, `created`, `accessed`, `enabled`, `activation`) VALUES
+(1, 'admin', 'Alex', 'Giokas', 'a.gkiokas@ortelio.co.uk', '486d18ed96603f0bbae2480e2c98cc80750402b28c1d4069d5df7c570ded0307', 0, '2014-11-15 18:01:34', '0000-00-00 00:00:00', 1, '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'),
+(7, 'bsmith', 'Bob', 'Smith', 'alexge233@hotmail.com', '7b4b075939a9c6823f85c35c19d029e2f91e8db8eabdd96a4cc0b4c8328df1c3', 2, '2014-11-17 03:18:03', '0000-00-00 00:00:00', 0, 'œK³Ý	¾@4°±œK‘{ÛH');
 
 --
 -- Constraints for dumped tables
@@ -136,8 +147,8 @@ ALTER TABLE `tblRapp`
 -- Constraints for table `tblRobot`
 --
 ALTER TABLE `tblRobot`
-  ADD CONSTRAINT `tblRobot_ibfk_2` FOREIGN KEY (`model`) REFERENCES `tblModel` (`id`),
-  ADD CONSTRAINT `tblRobot_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `tblUser` (`id`);
+  ADD CONSTRAINT `tblRobot_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `tblUser` (`id`),
+  ADD CONSTRAINT `tblRobot_ibfk_2` FOREIGN KEY (`model`) REFERENCES `tblModel` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
