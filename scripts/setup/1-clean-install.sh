16247a5109360223fb5a8fb8264df085a0ebc556
Manos Tsardoulias <etsardou@gmail.com>
2014-12-07 18:18:57 +0100

    Update 1-clean-install.sh

--- scripts/setup/1-clean-install.sh
+++ scripts/setup/1-clean-install.sh
@@ -60,7 +60,7 @@ sudo apt-get install -y git gitg
 
 echo -e "\e[1m\e[103m\e[31m [RAPP] Installing HOP by deb \e[0m"
 
-sudo sh -c 'echo "deb ftp://ftp-sop.inria.fr/indes/rapp/UBUNTU lts hop" > \
+sudo sh -c 'echo "deb ftp://ftp-sop.inria.fr/indes/rapp/UBUNTU lts hop" >> \
   /etc/apt/sources.list'
   
 sudo apt-get update
