b99315e5934fcc41aa8f6c784b812910fa8990a4
Manos Tsardoulias <etsardou@gmail.com>
2014-12-07 18:05:04 +0100

    Update 2-git-repos-setup.sh

--- scripts/setup/2-git-repos-setup.sh
+++ scripts/setup/2-git-repos-setup.sh
@@ -14,12 +14,16 @@
 echo -e "\e[1m\e[103m\e[31m [RAPP] Create Github folders \e[0m"
 # Create folder for RAPP platform repo
 cd ~/Desktop
-mkdir rapp-repos && cd rapp-repos
+mkdir rapp-platform-catkin-ws
+cd rapp-platform-catkin-ws
+mkdir src
+cd src
 
 echo -e "\e[1m\e[103m\e[31m [RAPP] HOP - Cloning the rapp-platform repo \e[0m"
 # Clone the repository (public key should have been setup)
 git clone git@github.com:rapp-project/rapp-platform.git
 
+catkin_init_workspace
 
 echo -e "\e[1m\e[103m\e[31m [RAPP] Step 2 finished. Now the HOP setup must be performed. \e[0m"
 
