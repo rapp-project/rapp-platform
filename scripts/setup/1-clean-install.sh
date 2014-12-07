7b2a7010af6161a87b08385599f17db5ff40ec07
Manos Tsardoulias <etsardou@gmail.com>
2014-12-07 17:38:15 +0100

    Update 1-clean-install.sh

--- scripts/setup/1-clean-install.sh
+++ scripts/setup/1-clean-install.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 
 echo -e "\e[1m\e[103m\e[31m [RAPP] Upgrading packages \e[0m"
 # Upgrades the current packages
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 #-------------------------------- ROS setup ----------------------------------#
 
