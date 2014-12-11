5e95942eb83688508ee0703ff135edb1398eb2fc
klpanagi <klpanagi@klpanagi-14.04LTS>
2014-12-11 16:50:02 +0200

    "Created branch, added documentation"

--- ric/hop_services/samples/fileTransfer/client.js
+++ ric/hop_services/samples/fileTransfer/client.js
@@ -1,22 +1,25 @@
 var fs = require ('fs');
+
+/* <Require writeFileSync(PATH,DATA) method> */
 var fsbin = require( "./fsbinary.js" );
 
-//console.log( "fsbin=", fsbin );
+console.log( "fsbin=", fsbin );
 
 import service serveFile (name);
 
 serveFile ("ice.jpg").post(
-    function (string)
-    {
-      console.log ('file downloaded');
-      fsbin.writeFileSync ('ice_copy.jpg', string );
-    },
-    {
-      asynchronous: false,  //wait for service to return
-      host: "localhost",  //localhost to run locally
-      port: 9001,
-      //user: "rapp",     //enable these for authentication
-      //password: "R@pPpaSS!",
-      fail: function( err ) { console.log( " connection refused: ", err ) }
-    }
-  );
+  function (data)
+  {
+    console.log ('file downloaded');
+    fsbin.writeFileSync ('ice_copy.jpg', data);
+  },
+  {
+    asynchronous: false,  //wait for service to return
+    host: "localhost",  //localhost to run locally
+    port: 9001,
+    /* <Define the below authentication params to access with server> */    
+    //user: "rapp", 
+    //password: "R@pPpaSS!",
+    fail: function( err ) { console.log( "Connection refused: ", err ) }
+  }
+);
--- ric/hop_services/samples/fileTransfer/serveFile.js
+++ ric/hop_services/samples/fileTransfer/serveFile.js
@@ -3,5 +3,6 @@ var hop = require ('hop');
 service serveFile (name) {
   var file = serveFile.resource (name);
   console.log ('serving file %s', file);
+  /* <Returns the requested file (data)> */
   return hop.HTTPResponseFile (file);
 }
