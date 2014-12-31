1e25cd7c832ccb617a72e971549f53b0706a1681
klpanagi <klpanagi@gmail.com>
2014-12-31 20:59:03 +0200

    Added random unigue string generator

--- ric/hop_services/services/qr.service.js
+++ ric/hop_services/services/qr.service.js
@@ -22,10 +22,23 @@ var qrRosService = "/ric/ros_nodes/qr_detection_service";
 var storePath = "/home/klpanagi/hop_temps/"; 
 
 /*---Initiatess Communication with RosBridge (Global)---*/
-var ros = new RosUtils();
-ros.init_bridge('');
+//var ros = new RosUtils();
+//ros.init_bridge('');
 /*------------------------------------------------------*/
+var cache = new Object();
+var cacheIdx = -1;
 
+   
+function randomStringGen(_length, _charsArray) {
+  var chars = _charsArray || "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  var string_length = _length || 5;
+  var randomString = "";
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomString += chars.substring(rnum,rnum+1);
+  }
+  return randomString;
+}
 
 /*!
  * @brief QR Node HOP Service Core.
@@ -33,21 +46,30 @@ ros.init_bridge('');
  */
 service qrNode (_qrImage)
 {
-  
-  var fileName = "qrImage.jpg";
+
+  var ros = new RosUtils();
+  ros.init_bridge('');
+  var randStr = "";
+  cacheIdx = cacheIdx + 1;
+  //randStr = randomStringGen(5);
+  //while( cache[randStr] != undefined )
+  do{
+    randStr = randomStringGen(5);
+
+  }
+  while ( cache[randStr.toString()] != undefined );
+  cache[randStr.toString()] = true; //true means exist.
+  console.log(cache);
+
+  var fileName = "qrImage-" + randStr + ".jpg";
   var qrFoundMessage = false;
 
   console.log("\033[01;36mRequest for qrNode service\033[0;0m");
   
-  var currentTime = new Date().getTime();
-  var header = {
-    "seq": 1,
-    "stamp": currentTime,
-    "frame_id": " "
-  };
+   
   var qrImagePath = Fs.resolvePath( storePath + fileName );
   var _args = {
-    "header": header,
+    //"header": header,
     "imageFilename": qrImagePath //filenamePATH    
   }; 
 
@@ -57,6 +79,8 @@ service qrNode (_qrImage)
   var returnMessage = ros.callService(qrRosService, _args);
   /*--<Removes the file after return status from rosbridge>--*/
   Fs.rmFileSync( storePath + fileName );
+  
+  delete cache[randStr.toString()];
   /*--<Returned message from qr ROS service>--*/
   return returnMessage; 
 }
