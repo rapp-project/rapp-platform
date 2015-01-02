690a5697c61431d8bc0fb7990e3dbb09d209270a
klpanagi <klpanagi@gmail.com>
2015-01-02 19:18:10 +0200

    Created test for upServices hop service

---
+++ ric/hop_services/tests/hop_services/upServices_test.js
@@ -0,0 +1,19 @@
+
+import service upServices ();
+
+var user = process.env.LOGNAME;
+var rapp_hop_path = "/home/" + user + "/Desktop/rapp-platform-catkin-ws/src/rapp-platform/ric/hop_services/";
+
+var ServerParams = require(rapp_hop_path + "utilities/./ServerParams.js");
+var params = new ServerParams(false, "155.207.19.37", 9001, "rappdev", "rappdev");
+
+var services = upServices().post(
+  function (data){
+    console.log( 'Received upRunning services names' );
+    return data;
+  },
+  params
+);
+
+console.log(services);
+
