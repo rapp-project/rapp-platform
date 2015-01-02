dad1119e43ee8cf0877a5a743da105b50c20083d
klpanagi <klpanagi@gmail.com>
2015-01-02 18:37:15 +0200

    Implemented track of currently running services in a .txt file

--- ric/hop_services/services/runWorkers.js
+++ ric/hop_services/services/runWorkers.js
@@ -10,10 +10,11 @@ var rapp_hop_path = "/home/" + user +
   "/rapp_platform_catkin_ws/src/rapp-platform/ric/hop_services/";
 
 var Fs = require( rapp_hop_path + "utilities/./fileUtils.js" );
+//var fs = require( 'fs' );
 var Path = require('path');
 
 var fileList = Fs.getFilesListSync( rapp_hop_path + "services" );
-var services = [];
+var Services = [];
 
 for (var i in fileList){
   //console.log( fileList[i] );
@@ -21,16 +22,23 @@ for (var i in fileList){
     console.log("Found js extension file: [%s]", fileList[i]);
     var regexp = /.service/g;
     if ( fileList[i].match(regexp) ){
-      services.push( fileList[i] );
+      Services.push( fileList[i] );
     } 
   }
 }
 
-console.log("\033[0;36mService js files found:\n\033[0;0m", services);
+console.log("\033[0;36mService js files found:\n\033[0;0m", Services);
         
 var Workers = new Array();
 
-for (var i in services){
-  Workers.push( new Worker ( "./" + services[i] ) );
-  console.log( "\033[01;35mCreated worker for service: [%s]", services[i] );
+Fs.rmFileSync( 'availableServices.txt' );
+
+for (var i in Services){
+  Workers.push( new Worker ( "./" + Services[i] ) );
+  console.log( "\033[01;35mCreated worker for service: [%s]\033[0;0m", Services[i] );
+
+  /*-----<Store available uprunning services on a txt file>-----*/
+  var regexp = /.service.js/g;
+  var str = Services[i].toString().replace( regexp, '' );
+  Fs.writeLine ( str,  "availableServices.txt" );  
 }
