c2206e3b43fe6e71f8ead44200073204246887f7
klpanagi <klpanagi@gmail.com>
2015-01-15 16:29:16 +0200

    Working on dummy services

--- ric/hop_services/services/qrDetection.service.js
+++ ric/hop_services/services/qrDetection.service.js
@@ -1,6 +1,6 @@
 /*!
- * @file faceNode.service.js
- * @brief Face detection service running on Remote host.
+ * @file qr.service.js
+ * @brief QR service running on Remote host.
  *
  */
 
@@ -14,12 +14,10 @@ var rapp_hop_path = "/home/" + user
 var Fs = require( /*rapp_hop_path +*/ "../utilities/./fileUtils.js" );
 var RosUtils = require( /*rapp_hop_path +*/ "../utilities/./RosUtils.js" );
 var RandStringGen = require ( /*rapp_hop_path +*/ "../utilities/./randStringGen.js" );
-var HopUtils = require( '../utilities/./HopServiceUtils.js' ); 
-
 /*----------------------------------------------*/
 
 /*-----<Defined Name of QR Node ROS service>----*/
-var faceRosService = "/ric/face_detection_service";
+var qrRosService = "/ric/ros_nodes/qr_detection_service";
 
 /*--<Defines the directory where images received are stored>--*/
 var storePath = "/home/klpanagi/hop_temps/"; 
@@ -33,36 +31,35 @@ var storePath = "/home/klpanagi/hop_temps/";
 var stringLength = 5;
 var randStrGen = new RandStringGen( stringLength );
 /*------------------------------------------------*/
-var hsu = new HopUtils();
-
 
 
 /*!
  * @brief QR Node HOP Service Core.
  * @param _qrImage Image data in BINARY encoding/format.
  */
-service face_byPath ( _imageURL, _clientParams )
+service qrDetection ( _data )
 {
 
   var randStr = randStrGen.createUnique();
-  var fileName = "faceImage-" + randStr + ".png";
+  var fileName = "qrImage-" + randStr + ".jpg";
+  var qrFoundMessage = false;
 
-  console.log("\033[01;36mRequest for face_byPath service\033[0;0m");
+  console.log("\033[01;36mRequest for qrDetection service\033[0;0m");
   
    
-  var imageStorePath = Fs.resolvePath( storePath + fileName );
+  var qrImagePath = Fs.resolvePath( storePath + fileName );
   var args = {
     //"header": header,
-    "imageFilename": imageStorePath   
+    "imageFilename": qrImagePath //filenamePATH    
   }; 
 
-  /*-----<Request File and Stores >-----*/
-  hsu.getFile( _imageURL, imageStorePath, _clientParams );
+  /*-----<Stores received image data>-----*/
+  //Fs.writeBinFileSync( qrImagePath, _data );
 
   /*-----<Call QR ROS service through rosbridge>-----*/
   //var ros = new RosUtils();
   //ros.init_bridge('');
-  //var returnMessage = ros.callService( faceRosService, args );
+  //var returnMessage = ros.callService( qrRosService, args );
   var returnMessage = "I am a dummy message";
 
   /*--<Removes the file after return status from rosbridge>--*/
