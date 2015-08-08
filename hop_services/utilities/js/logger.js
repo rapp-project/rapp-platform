4a742331eee1e3a5695c915d2740d686af22fca7
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-08-08 21:09:36 +0300

    Enhanced logger module

--- hop_services/utilities/js/logger.js
+++ hop_services/utilities/js/logger.js
@@ -6,6 +6,11 @@ var Fs = require( './fileUtils.js' );
 
 var __logDir = '~/.hop/log/'
 
+var __logDirCreated = false;
+
+var __logFiles = [];
+
+
 /*!
  * @brief Sets the direcory where log files are stored
  */
@@ -16,13 +21,50 @@ function setLogDir(logDir)
 
 
 /*!
+ * @brief Creates directory where we store log files (.log).
+ */
+function createLogDir(logDir)
+{
+  if(logDir == undefined || logDir == '')
+  {
+    Fs.createDirRecur(__logDir);
+  }
+  else
+  {
+    Fs.createDirRecur(logDir);
+  }
+  __logDirCreated = true;
+  return;
+}
+
+
+/*!
  * @brief Instantiates log file of given worker
  */
 function createLogFile(workerName)
 {
-  Fs.createDir(__logDir);
+  if(__logDirCreated == false)
+  {
+    console.log('Must create log directory first by calling the' +
+      'relevant method [createLogDir]');
+    return false;
+  }
+
   var logFilePath = __logDir + workerName + '.log';
-  Fs.write_file_sync(logFilePath, "");
+
+  if ( logFileExists(logFilePath) )
+  {
+    console.log('Log File already exists!!! Stopping creation');
+    return false;
+  }
+
+  if ( Fs.write_file_sync(logFilePath, "") == true )
+  {
+    __logFiles.push(logFilePath);
+    return true;
+  }
+
+  return false;
 };
 
 
@@ -32,13 +74,33 @@ function createLogFile(workerName)
 function appendToLogFile(workerName, log)
 {
   var logFilePath = __logDir + workerName + '.log';
-  Fs.writeLine(log, logFilePath);
+
+  if ( logFileExists(logFilePath) )
+  {
+    Fs.writeLine(log, logFilePath);
+    return true;
+  }
+
+  console.log('Requested write to log file [%s] is not permitted' +
+    'because log file was not previously created', logFilePath);
+  return false;
 };
+
+
+/*!
+ * @brief Checks if service given exists in registered services
+ */
+function logFileExists(logFile)
+{
+  if (__logFiles.indexOf(logFile) > -1) {return true;}
+  else {return false;}
+}
 
 
 // Module Exports
 module.exports = {
   appendToLogFile: appendToLogFile,
   createLogFile: createLogFile,
+  createLogDir: createLogDir,
   setLogDir: setLogDir
 }
