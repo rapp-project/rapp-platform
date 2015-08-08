f6c66a510592392d22221cf1a1f76bc97f2bc7a0
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-08-08 21:10:23 +0300

    Implemented getDate() + getTime() + getUpTime() methods

--- hop_services/utilities/js/runtime.js
+++ hop_services/utilities/js/runtime.js
@@ -5,12 +5,50 @@
 
 var startT = process.hrtime();
 
-function get_time()
+
+/*!
+ * @brief Returns a number that represents time this module is up.
+ * High Precision
+ */
+function getUpTime()
 {
   var upTime = process.hrtime(startT);
   return upTime[0] * 1e3 + upTime[1] / 1e6;
 }
 
+
+/*!
+ * @brief Returns a number that represents the number of milliseconds since
+ * midnight January 1, 1970.
+ */
+function getTime()
+{
+  return new Date().getTime();
+}
+
+
+/*!
+ * @brief Returns a String that represents current Date + time
+ */
+function getDate()
+{
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {dd = '0' + dd;}
+
+  if(mm<10) {mm = '0' + mm;}
+
+  fullDate = mm + '-' + dd + '-' + yyyy;
+
+  time = today.getTime();
+
+  return fullDate + '-' + time;
+}
+
 module.exports = {
-  get_time: get_time
+  getTime: getTime,
+  getDate: getDate
 }
