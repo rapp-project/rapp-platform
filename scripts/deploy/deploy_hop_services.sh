18c4637b3ca49a6c9986c09aefdcc70590ed27b2
Alex Ge <alexge233@hotmail.com>
2015-08-09 22:17:24 +0200

    fixed home variable and rapp platform dir name

--- scripts/deploy/deploy_hop_services.sh
+++ scripts/deploy/deploy_hop_services.sh
@@ -5,15 +5,15 @@ export PATH=$PATH:/opt/bigloo/bin:/opt/hop/bin
 
 #------- ROS related
 source /opt/ros/indigo/setup.bash
-source /home/etsardou/knowrob_catkin_ws/devel/setup.bash --extend
-source /home/etsardou/rosjava/devel/setup.bash --extend
-source /home/etsardou/rapp_platform_catkin_ws/devel/setup.bash --extend
+source $HOME/knowrob_catkin_ws/devel/setup.bash --extend
+source $HOME/rosjava/devel/setup.bash --extend
+source $HOME/rapp-platform-catkin-ws/devel/setup.bash --extend
 
 #------- KnowRob related
 export JAVA_HOME=/usr/lib/jvm/default-java
 export SWI_HOME_DIR=/usr/lib/swi-prolog
 export LD_LIBRARY_PATH=/usr/lib/jvm/default-java/jre/lib/amd64:/usr/lib/jvm/default-java/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
-HOP_SERVICES_PATH=/home/etsardou/rapp_platform_catkin_ws/src/rapp-platform/hop_services/services
+HOP_SERVICES_PATH=$HOME/rapp-platform-catkin-ws/src/rapp-platform/hop_services/services
 
 hop -p 9001 $HOP_SERVICES_PATH/runWorkers.js
