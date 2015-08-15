789ca5f9263f48a88f1d155f1973c729a3c0b8c0
Manos Tsardoulias <etsardou@gmail.com>
2015-08-15 12:03:48 +0300

    unit tests for the english support

--- rapp_speech_detection_sphinx4/tests/unit/english_support_unit_tests.py
+++ rapp_speech_detection_sphinx4/tests/unit/english_support_unit_tests.py
@@ -16,12 +16,81 @@ class TestAudioProcessing(unittest.TestCase):
     def tearDown(self):
         self.utilities_module = None
 
-    def test_mixedCases(self):
-        # files = []
-        # files.append("/tmp/file_1")
-        # files.append("/tmp/file_2")
-        # open(files[0], 'w')
+    def test_genericConfigurationFiles(self):
+        conf = self.english_support_module.getGenericConfiguration()
+        
+        jar_path = conf['jar_path']
+        jar_path = jar_path.split(':')
+        self.assertEqual(jar_path[0], '.')
+        self.assertEqual(os.path.isfile(jar_path[1]), True)
+        self.assertEqual(os.path.isdir(jar_path[2]), True)
 
-        # result = self.utilities_module.cleanup(files)
-        self.assertEqual(True, True)
+        conf_path = conf['configuration_path']
+        self.assertEqual(os.path.isfile(conf_path), True)
 
+        acoustic = conf['acoustic_model']
+        self.assertEqual(os.path.isdir(acoustic), True)
+
+        grammar_folder = conf['grammar_folder']
+        self.assertEqual(os.path.isdir(grammar_folder), True)
+
+        dictionary = conf['dictionary']
+        self.assertEqual(os.path.isfile(dictionary), True)
+
+        language_model = conf['language_model']
+        self.assertEqual(os.path.isfile(language_model), True)
+
+    def test_limitedVocabularyConfigurationFiles_correctCase(self):
+        conf = self.english_support_module.getLimitedVocebularyConfiguration(\
+                ['this', 'is', 'a', 'test'],\
+                [],\
+                [])
+        
+        jar_path = conf['jar_path']
+        jar_path = jar_path.split(':')
+        self.assertEqual(jar_path[0], '.')
+        self.assertEqual(os.path.isfile(jar_path[1]), True)
+        self.assertEqual(os.path.isdir(jar_path[2]), True)
+
+        conf_path = conf['configuration_path']
+        self.assertEqual(os.path.isfile(conf_path), True)
+
+        acoustic = conf['acoustic_model']
+        self.assertEqual(os.path.isdir(acoustic), True)
+
+        grammar_folder = conf['grammar_folder']
+        self.assertEqual(os.path.isdir(grammar_folder), True)
+
+        dictionary = conf['dictionary']
+        self.assertEqual(os.path.isfile(dictionary), True)
+
+        language_model = conf['language_model']
+        self.assertEqual(os.path.isfile(language_model), True)
+ 
+    def test_limitedVocabularyConfigurationFiles_notExistentWords(self):
+        # This produces the proper files without the erroneous words
+        conf = self.english_support_module.getLimitedVocebularyConfiguration(\
+                ['kakakakaka', 'lslslslsl', 'a', 'test'],\
+                [],\
+                [])
+        
+        jar_path = conf['jar_path']
+        jar_path = jar_path.split(':')
+        self.assertEqual(jar_path[0], '.')
+        self.assertEqual(os.path.isfile(jar_path[1]), True)
+        self.assertEqual(os.path.isdir(jar_path[2]), True)
+
+        conf_path = conf['configuration_path']
+        self.assertEqual(os.path.isfile(conf_path), True)
+
+        acoustic = conf['acoustic_model']
+        self.assertEqual(os.path.isdir(acoustic), True)
+
+        grammar_folder = conf['grammar_folder']
+        self.assertEqual(os.path.isdir(grammar_folder), True)
+
+        dictionary = conf['dictionary']
+        self.assertEqual(os.path.isfile(dictionary), True)
+
+        language_model = conf['language_model']
+        self.assertEqual(os.path.isfile(language_model), True)
