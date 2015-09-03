7bb13c8393e45add5a2b606205b0aa5fc9c020fe
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-09-03 19:35:02 +0300

    Create and hold write streams for writing to log files -- Fixed bug on fast writing to log files

--- hop_services/modules/WorkerHandler/logger.js
+++ hop_services/modules/WorkerHandler/logger.js
@@ -3,6 +3,7 @@
  */
 
 var Fs = require( '../fileUtils.js' );
+var FileStreams = require( '../fileStreams.js' );
 var sleep = require( '../sleep.js' );
 
 // Default log directory path
@@ -10,6 +11,7 @@ var __logDir = '~/.hop/log/'
 
 var __logDirCreated = false;
 var __logFiles = [];
+var __logFileStreams = {};
 
 
 /*!
@@ -46,6 +48,7 @@ function createLogDir(logDir)
  */
 function createLogFile( workerName )
 {
+  // Check if there the log directory was previously created.
   if( __logDirCreated == false )
   {
     console.log('Must create log directory first by calling the' +
@@ -55,15 +58,19 @@ function createLogFile( workerName )
 
   var logFilePath = __logDir + workerName + '.log';
 
+  // Check if the log file was previously created.
   if ( logFileExists(logFilePath) )
   {
     console.log('Log File already exists!!! Stopping creation');
     return false;
   }
 
+  // Evaluate write methods on given log file.
   if ( Fs.write_file_sync(logFilePath, "") == true )
   {
     __logFiles.push(logFilePath);
+    // Create and initiate a new write stream for this logfile.
+    __logFileStreams[logFilePath] = new FileStreams.WriteStream(logFilePath);
     return true;
   }
 
@@ -81,20 +88,7 @@ function appendToLogFile(workerName, logMsg)
 
   if ( logFileExists(logFilePath) )
   {
-    /**
-     * Force current thread to sleep. Fast i/o calls by the logger causes
-     * an assertion in fs.c:
-     *
-     * hop: src/unix/fs.c:827: void uv__fs_done(struct uv__work *, int):
-     * Assertion `(((const QUEUE *) (&(req->loop)->active_reqs) ==
-     * (const QUEUE *) (*(QUEUE **) &((*(&(req->loop)->active_reqs))[0])))
-     * == 0)' failed.
-     *
-     * @TODO Create and hold a writeStream for each logfile.
-     *
-     */
-    sleep.sleepMS(150);
-    Fs.appendLine(logMsg, logFilePath);
+    __logFileStreams[logFilePath].write(logMsg + '\n');
     return true;
   }
 
