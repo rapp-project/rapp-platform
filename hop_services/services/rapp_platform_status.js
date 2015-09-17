f1660cd205d20cbee8b02803dca4dc0df14e0e5a
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-09-17 23:28:22 +0300

    Initial commit of service that delivers platform status html

---
+++ hop_services/services/rapp_platform_status.js
@@ -0,0 +1,46 @@
+/*!
+ * @file rapp_platform_status.service.js
+ * @brief Returns available hop services located on RAPP Platform
+ */
+
+
+/**
+ *  MIT License (MIT)
+ *
+ *  Copyright (c) <2014> <Rapp Project EU>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ *
+ *  Authors: Konstantinos Panayiotou
+ *  Contact: klpanagi@gmail.com
+ *
+ */
+
+
+/*--------------Load required modules-----------*/
+var hop = require('hop');
+/*----------------------------------------------*/
+
+var domTree = require( '../html/rapp_platform_ros_conditions.html' );
+
+service platform_status(  )
+{
+  return domTree;
+}
