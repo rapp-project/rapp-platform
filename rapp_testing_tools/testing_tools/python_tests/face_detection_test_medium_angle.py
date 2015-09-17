b5ae760b4bf228c957153873fe319a1a4f9135da
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-09-17 22:09:57 +0300

    Created face_detection scenario tests -- close/medium/far -- straight/angle

---
+++ rapp_testing_tools/testing_tools/python_tests/face_detection_test_medium_angle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#MIT License (MIT)
+
+#Copyright (c) <2014> <Rapp Project EU>
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+# Authors: Konstantinos Panayiotou, Manos Tsardoulias
+# contact: klpanagi@gmail.com, etsardou@iti.gr
+
+import os
+import timeit
+
+__path__ = os.path.dirname(os.path.realpath(__file__))
+
+## ------ Access the RappCloud python module ------- ##
+from RappCloud import RappCloud
+
+class RappInterfaceTest:
+
+  def __init__(self):
+    self.rappCloud = RappCloud()
+    self.file_uri = __path__  + '/../test_data/face_samples/fatsaSou_medium_angle.jpg'
+    self.valid_faces = [{
+        'up_left_point': {'y': 153.0, 'x': 118.0},
+        'down_right_point': {'y': 320.0, 'x': 285.0}
+    }]
+
+
+  def execute(self):
+    start_time = timeit.default_timer()
+    response = self.rappCloud.face_detection(self.file_uri)
+    end_time = timeit.default_timer()
+    self.elapsed_time = end_time - start_time
+    return self.validate(response)
+
+  def validate(self, response):
+    error = response['error']
+    if error != "":
+      return [error, self.elapsed_time]
+
+    faces = response['faces']
+    if self.valid_faces == faces:
+      return [True, self.elapsed_time]
+    else:
+      return ["Unexpected result : " + str(response), self.elapsed_time]
+
