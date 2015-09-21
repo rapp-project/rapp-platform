7333d1c1d7b9171ad7a8912316b2c89a0bf9def6
Thanos Kintsakis <thanos@rapp.ee.auth.gr>
2015-09-21 14:22:32 +0300

    cognitive_exercise .py node updated.. name changes in ontology, added cts tag to differentiate where needed

--- rapp_cognitive_exercise/src/cognitive_exercise.py
+++ rapp_cognitive_exercise/src/cognitive_exercise.py
@@ -35,6 +35,7 @@ import time
 
 from datetime import datetime
 from os.path import expanduser
+from test_selector import TestSelector
 
 from rapp_platform_ros_communications.srv import (
   testSelectorSrv,  
@@ -60,75 +61,12 @@ class CognitiveExercise:
     if(not self.serv_topic):
       rospy.logerror("rapp_cognitive_exercise_chooser_topic not found")   
     self.serv=rospy.Service(self.serv_topic, testSelectorSrv, self.chooserDataHandler) 
-    
-    
-  #def getOntologyAlias(self,userid):
-    #serv_topic = rospy.get_param('rapp_mysql_wrapper_robot_fetch_data_topic')
-    #if(not serv_topic):
-      #rospy.logerror("mysql_wrapper_robot_read_data_topic")
-    #rospy.wait_for_service(serv_topic)
-    #db_service = rospy.ServiceProxy(serv_topic, fetchDataSrv)
-    #req = fetchDataSrv()
-    #req.req_cols=[]
-    #entry1=[]
-    #req.where_data=[StringArrayMsg(s=entry1)]
-    #response = db_service(req.req_cols,req.where_data)
-    #self.assertEqual(response.trace[0],"Wrong Query Input Format, check for empty required columns list or wrong/incomplete Query data format")
-    #self.assertFalse(response.success.data)     
-    
-  def chooserFunction(self,req):
-    try:
-      res = testSelectorSrvResponse()      
-      #check if username exists, if not prompt for create user in mysql DB
-      #check if ontology alias exists for user, if not create it
-      serv_topic = rospy.get_param('rapp_knowrob_wrapper_create_ontology_alias')
-      if(not serv_topic):
-        rospy.logerror("mysql_wrapper_rapp_read_data topic param not found")
-      rospy.wait_for_service(serv_topic)
-      knowrob_service = rospy.ServiceProxy(serv_topic, createOntologyAliasSrv)
-      req = createOntologyAliasSrvRequest()
-      req.username="admin"      
-      response = knowrob_service(req)
-      res.trace.append(response.ontology_alias)
-      
-      #choose category if not defined (arithemtic, recall etc)
-      #retrieve past performance from ontology for specified category
-      
-      tmp=expanduser('~')    
-      res.trace.append(tmp)
-      timestamp = str(int(time.time()))
-      edibleTime=datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-      res.trace.append(edibleTime)
-      res.trace.append(timestamp)     
-      
-      
-      testFilePath=tmp+"/rapp_platform_catkin_ws/src/rapp-platform/rapp_cognitive_exercise/cognitiveTests/arithmeticTest1.xml"
-      tree = ET.parse(testFilePath)
-      root = tree.getroot()
-      
-      for question in root.find('Questions'):        
-        res.questions.append(question.find("body").text)
-        line=StringArrayMsg()
-        for answers in question.findall('answer'):          
-          line.s.append(answers.find("body").text)        
-        res.answers.append(line)
-            
-    
-      #check if ontology alias of user exists      
-    except IndexError:
-      res.trace.append("Wrong Query Input Format, check for empty required columns list or wrong/incomplete Query data format")
-      res.success.data=False
-      #print "Wrong Query Input Format, check for empty required columns list or wrong/incomplete Query data format"
-    except IOError:
-      print "Error: can\'t find login file or read data" 
-      res.success.data=False
-      res.trace.append("Error: can\'t find login file or read data")
-    return res
-                
+                    
   #tblUser callbacks    
   def chooserDataHandler(self,req):     
     res = testSelectorSrvResponse()
-    res=self.chooserFunction(req) 
+    it = TestSelector()    
+    res=it.chooserFunction(req) 
     return res    
     
 if __name__ == "__main__": 
--- rapp_cognitive_exercise/src/test_selector.py
+++ rapp_cognitive_exercise/src/test_selector.py
@@ -34,11 +34,17 @@ from datetime import datetime
 from os.path import expanduser
 
 from rapp_platform_ros_communications.srv import (
+  ontologySubSuperClassesOfSrv,
+  ontologySubSuperClassesOfSrvRequest,
+  ontologySubSuperClassesOfSrvResponse,
   testSelectorSrv,  
   testSelectorSrvResponse,
   createOntologyAliasSrv,
   createOntologyAliasSrvRequest,
-  createOntologyAliasSrvResponse
+  createOntologyAliasSrvResponse,
+  userPerformanceCognitveTestsSrv,
+  userPerformanceCognitveTestsSrvRequest,
+  userPerformanceCognitveTestsSrvResponse
   )
   
 from rapp_platform_ros_communications.msg import ( 
@@ -47,56 +53,119 @@ from rapp_platform_ros_communications.msg import (
 
 class TestSelector:
 
-  def chooserFunction(self,username):
+  def chooserFunction(self,req):
     try:
       res = testSelectorSrvResponse()      
+      
+      #must insert code here to check if testType exists. use subclassesof function of knowrob wrapper, super class is CognitiveTests
+      serv_topic = rospy.get_param('rapp_knowrob_wrapper_subclasses_of_topic')
+      if(not serv_topic):
+        rospy.logerror("rapp_knowrob_wrapper_subclasses_of_topic topic param not found")
+        res.trace.append("rapp_knowrob_wrapper_subclasses_of_topic topic param not found")
+        res.error="rapp_knowrob_wrapper_subclasses_of_topic topic param not found"
+        res.success=False
+        return res
+      rospy.wait_for_service(serv_topic)
+      knowrob_service = rospy.ServiceProxy(serv_topic, ontologySubSuperClassesOfSrv)
+      testTypesReq = ontologySubSuperClassesOfSrvRequest()
+      testTypesReq.ontology_class="CognitiveTests" 
+      testTypesResponse = knowrob_service(testTypesReq)
+      if(testTypesResponse.success!=True):
+        res.trace=testTypesResponse.trace
+        res.trace.append("cannot load test categories from ontology")
+        res.error=testTypesResponse.error+"cannot load test categories from ontology"
+        res.success=False
+        return res
+      testTypesDict=dict()
+      for s in testTypesResponse.results:
+        #res.trace.append(s)
+        testTypesDict[s]=['1']
+      if ("http://knowrob.org/kb/knowrob.owl#"+req.testType not in testTypesDict): 
+        #res.trace.append(testTypesDict
+        res.trace.append("testType provided does not exist")
+        res.error="testType provided does not exist"
+        res.success=False
+        return res
+      
+      #testTypes=testTypesResponse.results
+      #if(req.testType!=""):
+        #if any(req.testType in s for s in testTypes):
+          #print "something"
+          
       #check if username exists, if not prompt for create user in mysql DB
       #check if ontology alias exists for user, if not create it
       serv_topic = rospy.get_param('rapp_knowrob_wrapper_create_ontology_alias')
       if(not serv_topic):
         rospy.logerror("mysql_wrapper_rapp_read_data topic param not found")
+        res.trace.append("mysql_wrapper_rapp_read_data topic param not found")
+        res.error="mysql_wrapper_rapp_read_data topic param not found"
+        res.success=False
+        return res
       rospy.wait_for_service(serv_topic)
       knowrob_service = rospy.ServiceProxy(serv_topic, createOntologyAliasSrv)
-      req = createOntologyAliasSrvRequest()
-      req.username=username    
-      response = knowrob_service(req)
-      if(response.success!=True):
-        res.trace=response.trace
-        res.error=response.error
+      createOntologyAliasReq = createOntologyAliasSrvRequest()
+      createOntologyAliasReq.username=req.username    
+      createOntologyAliasResponse = knowrob_service(createOntologyAliasReq)
+      if(createOntologyAliasResponse.success!=True):
+        res.trace=createOntologyAliasResponse.trace
+        res.error=createOntologyAliasResponse.error
         res.success=False
         return res
       
-      
+      serv_topic = rospy.get_param('rapp_knowrob_wrapper_user_performance_cognitve_tests')
+      if(not serv_topic):
+        rospy.logerror("rapp_knowrob_wrapper_user_performance_cognitve_tests")
+        res.trace.append("mysql_wrapper_rapp_read_data topic param not found")
+        res.error="mysql_wrapper_rapp_read_data topic param not found"
+        res.success=False
+        return res
+     
+      if (req.testType==""):
+        print "kati"
+      else:        
+        userPerformanceReq=userPerformanceCognitveTestsSrvRequest()
+        userPerformanceReq.test_type=req.testType
+        userPerformanceReq.ontology_alias=createOntologyAliasResponse.ontology_alias
+        knowrob_service = rospy.ServiceProxy(serv_topic, userPerformanceCognitveTestsSrv)       
+        userPerformanceResponse = knowrob_service(userPerformanceReq)
+        if(userPerformanceResponse.success!=True):
+          #means no performance records exist. so you choose random test, test1, variation1
+          res.trace=userPerformanceResponse.trace
+          res.error=userPerformanceResponse.error
+          res.success=False
+          return res
+        
       #choose category if not defined (arithemtic, recall etc)
       #retrieve past performance from ontology for specified category
       
-      tmp=expanduser('~')    
-      res.trace.append(tmp)
-      timestamp = str(int(time.time()))
-      edibleTime=datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-      res.trace.append(edibleTime)
-      res.trace.append(timestamp)     
-      
-      
-      testFilePath=tmp+"/rapp_platform_catkin_ws/src/rapp-platform/rapp_cognitive_exercise/cognitiveTests/arithmeticTest1.xml"
-      tree = ET.parse(testFilePath)
-      root = tree.getroot()
-      
-      for question in root.find('Questions'):        
-        res.questions.append(question.find("body").text)
-        line=StringArrayMsg()
-        for answers in question.findall('answer'):          
-          line.s.append(answers.find("body").text)        
-        res.answers.append(line)
+      #tmp=expanduser('~') 
+        tmp=userPerformanceResponse.tests[0]
+        res.trace.append(tmp)
+        timestamp = str(int(time.time()))
+        edibleTime=datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        res.trace.append(edibleTime)
+        res.trace.append(timestamp)     
+        
+        
+        testFilePath=tmp+"/rapp_platform_catkin_ws/src/rapp-platform/rapp_cognitive_exercise/cognitiveTests/arithmeticTest1.xml"
+        tree = ET.parse(testFilePath)
+        root = tree.getroot()
+        
+        for question in root.find('Questions'):        
+          res.questions.append(question.find("body").text)
+          line=StringArrayMsg()
+          for answers in question.findall('answer'):          
+            line.s.append(answers.find("body").text)        
+          res.answers.append(line)
             
     
       #check if ontology alias of user exists      
     except IndexError:
       res.trace.append("Wrong Query Input Format, check for empty required columns list or wrong/incomplete Query data format")
-      res.success.data=False
+      res.success=False
       #print "Wrong Query Input Format, check for empty required columns list or wrong/incomplete Query data format"
     except IOError:
       print "Error: can\'t find login file or read data" 
-      res.success.data=False
+      res.success=False
       res.trace.append("Error: can\'t find login file or read data")
     return res
