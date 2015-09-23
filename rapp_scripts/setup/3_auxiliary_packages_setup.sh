ee7539a68b6c83edc6a6f114c78348c2c29e2f80
Manos Tsardoulias <etsardou@gmail.com>
2015-09-23 20:20:19 +0300

    Minor scripts bug fixed

--- rapp_scripts/setup/3_auxiliary_packages_setup.sh
+++ rapp_scripts/setup/3_auxiliary_packages_setup.sh
@@ -4,4 +4,5 @@ echo -e "\e[1m\e[103m\e[31m [RAPP] Installing auxiliary packages \e[0m"
 sudo apt-get install -y openssh-server
 sudo apt-get install -y vim
 sudo apt-get install -y git gitg
+sudo apt-get install -y python-magic
 
