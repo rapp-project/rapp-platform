759d01901b41aef5a9b062f552a4f2098ba749b5
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-09-24 17:13:22 +0300

    Created platform_status service -- web-client

--- hop_services/services/rapp_platform_status.js
+++ hop_services/services/rapp_platform_status.js
@@ -36,11 +36,94 @@
 
 /*--------------Load required modules-----------*/
 var hop = require('hop');
+var module_path = '../modules/'
+var ROS = require( module_path + 'RosBridgeJS/src/Rosbridge.js' );
 /*----------------------------------------------*/
+var ros = new ROS('localhost','9090');
 
 var domTree = require( '../html/rapp_platform_ros_conditions.html' );
+var platformActiveServices = [];
+var platformActiveNodes = [];
+var rappRosServices  = [];
+var pollCycle = 1000; // 10 sec
+
+
+loopInf();
 
 service platform_status(  )
 {
-  return domTree;
+  var services_literal =
+    '<h2 align="center"><span class="label label-default">ROS Services</span></h2><br/><ul>';
+  var rapp_srv_literal =
+    '<h2 align="center"><span class="label label-default">RAPP ROS Services</span></h2><br/><ul>';
+  var nodes_literal =
+    '<h2 align="center"><span class="label label-default">ROS Nodes</span></h2><br/><ul>';
+
+  for(index = 0 ; index < platformActiveServices.length ; index++)
+  {
+    services_literal += "<li>" + platformActiveServices[index] + '</li>';
+  }
+  services_literal += '</ul>';
+
+  for(index = 0 ; index < rappRosServices.length ; index++)
+  {
+    rapp_srv_literal += "<li>" + rappRosServices[index] + '</li>';
+  }
+  rapp_srv_literal += '</ul>';
+
+  for(index = 0 ; index < platformActiveNodes.length ; index++)
+  {
+    nodes_literal += "<li>" + platformActiveNodes[index] + '</li>';
+  }
+  nodes_literal += '</ul>';
+
+
+  return <html>
+    <head>
+      <meta charset="utf-8"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    </head>
+    <body>
+      <div class='jumbotron' align='center'>
+        <h1>RAPP Platform information</h1>
+        <div>
+          To work, roscore and rosbridge_websocket must be executed
+        </div>
+      </div>
+      <div class='container'>
+        <div class='row-fluid'>
+          <div class='col-md-4' style='word-wrap:break-word;'>
+            <div id="nodes_div" class='panel panel-default'> ${nodes_literal} </div>
+          </div>
+          <div class='col-md-4' style='word-wrap:break-word;'>
+            <div id="services_div" class="panel panel-default"> ${services_literal} </div>
+          </div>
+          <div class='col-md-4' style='word-wrap:break-word;'>
+            <div id="rapp_services_div" class="panel panel-default"> ${rapp_srv_literal} </div>
+          </div>
+        </div>
+      </div>
+
+    </body>
+  </html>;
 }
+
+function loopInf(){
+  setTimeout(function(){
+    ros.getServices(function(data){
+      platformActiveServices = data;
+      for(i in platformActiveServices){
+        if(platformActiveServices[i].indexOf("rapp") > -1){
+          rappRosServices.push(platformActiveServices[i])
+        }
+      }
+    })
+    ros.getNodes(function(data){
+      platformActiveNodes = data;
+    })
+
+    loopInf();
+  }, pollCycle)
+}
+
+
