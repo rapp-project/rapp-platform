2c8cc32595548eb14288aca86a3e0990ef0e61d5
Konstantinos Panayiotou <klpanagi@gmail.com>
2015-09-26 18:58:20 +0300

    Loading rapp_platform_services + ros_nodes + ros_topics

--- hop_services/services/rapp_platform_status.service.js
+++ hop_services/services/rapp_platform_status.service.js
@@ -44,46 +44,79 @@ var ros = new ROS('localhost','9090');
 var domTree = require( '../html/rapp_platform_ros_conditions.html' );
 var platformActiveServices = [];
 var platformActiveNodes = [];
+var platformActiveTopics = [];
 var rappRosServices  = [];
 var pollCycle = 1000; // 10 sec
 
 
 loopInf();
 
-service platform_status(  )
+var navbar =
+    '<div class="navbar navbar-inverse navbar-fixed-top">' +
+      '<div class="navbar-inner">' +
+        '<div class="container">' +
+          '<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">' +
+            '<a class="btn btn-success" href="#">RAPP Platform Status</a>' +
+          '</button>' +
+          //'<a class="brand" href="#">Project name</a>' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+
+service rapp_platform_status(  )
 {
-  var services_literal =
-    '<h2 align="center"><span class="label label-default">ROS Services</span></h2><br/><ul>';
-  var rapp_srv_literal =
-    '<h2 align="center"><span class="label label-default">RAPP ROS Services</span></h2><br/><ul>';
-  var nodes_literal =
-    '<h2 align="center"><span class="label label-default">ROS Nodes</span></h2><br/><ul>';
+  var srv_class = (platformActiveServices.length > 1) ? "bg-success" : "bg-danger";
+  var nodes_class = (platformActiveNodes.length > 1) ? "bg-success" : "bg-danger";
+  var topics_class = (platformActiveTopics.length > 1) ? "bg-success" : "bg-danger";
+
+  var rapp_srv_literal = '';
+  var services_literal = '';
+  var nodes_literal = '';
+  var topics_literal = '';
+  var nodes_label =
+    '<h2 align="center"><p class=' + nodes_class +
+    ' align="center">ROS Nodes</p></h2>';
+  var rapp_services_label =
+    '<h2 align="center"><p class=' + srv_class +
+    ' align="center">ROS Services</p></h2>';
+  var topics_label =
+    '<h2 align="center"><p class=' + srv_class +
+    ' align="center">ROS Topics</p></h2>';
 
   for(index = 0 ; index < platformActiveServices.length ; index++)
   {
     services_literal += "<li>" + platformActiveServices[index] + '</li>';
   }
-  services_literal += '</ul>';
 
   for(index = 0 ; index < rappRosServices.length ; index++)
   {
-    rapp_srv_literal += "<li>" + rappRosServices[index] + '</li>';
+    rapp_srv_literal += "<option>" +   rappRosServices[index] + '</option>';
   }
-  rapp_srv_literal += '</ul>';
 
   for(index = 0 ; index < platformActiveNodes.length ; index++)
   {
-    nodes_literal += "<li>" + platformActiveNodes[index] + '</li>';
+    nodes_literal += "<option>" + platformActiveNodes[index] + '</option>';
   }
-  nodes_literal += '</ul>';
+
+  for(index = 0 ; index < platformActiveNodes.length ; index++)
+  {
+    topics_literal += "<option>" + platformActiveTopics[index] + '</option>';
+  }
 
 
-  return <html>
+  return <html lang="en">
     <head>
-      <meta charset="utf-8"/>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+      <meta charset="utf-8"/>
+      <meta name="viewpoint" content="width=device-width, initial-scale=1">
+      <style>
+        body {
+          padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+        }
+      </style>
     </head>
     <body>
+      ${navbar}
       <div class='jumbotron' align='center'>
         <h1>RAPP Platform information</h1>
         <div>
@@ -93,17 +126,40 @@ service platform_status(  )
       <div class='container'>
         <div class='row-fluid'>
           <div class='col-md-4' style='word-wrap:break-word;'>
-            <div id="nodes_div" class='panel panel-default'> ${nodes_literal} </div>
+            ${rapp_services_label}
+            <select id='SrvBox' class="form-control" onchange=~{
+              var selected = this.options[this.selectedIndex].value
+                alert(selected)
+              }>
+              ${rapp_srv_literal}
+            </select>
           </div>
           <div class='col-md-4' style='word-wrap:break-word;'>
-            <div id="services_div" class="panel panel-default"> ${services_literal} </div>
+            ${nodes_label}
+            <select id='NodesBox' class="form-control" onchange=~{
+              var selected = this.options[this.selectedIndex].value
+                alert(selected)
+              }>
+              ${nodes_literal}
+            </select>
           </div>
           <div class='col-md-4' style='word-wrap:break-word;'>
-            <div id="rapp_services_div" class="panel panel-default"> ${rapp_srv_literal} </div>
+            ${topics_label}
+            <select id='NodesBox' class="form-control" onchange=~{
+              var selected = this.options[this.selectedIndex].value
+                alert(selected)
+              }>
+              ${topics_literal}
+            </select>
           </div>
         </div>
       </div>
-
+      <div style="position: absolute; bottom: 5px;">
+        <address>
+          <strong>The RAPP-Project</strong><br>
+          <a href="rapp-project.eu">rapp-project.eu</a>
+        </address>
+      </div>
     </body>
   </html>;
 }
@@ -112,6 +168,8 @@ function loopInf(){
   setTimeout(function(){
     ros.getServices(function(data){
       platformActiveServices = data;
+      //console.log(data)
+      rappRosServices = [];
       for(i in platformActiveServices){
         if(platformActiveServices[i].indexOf("rapp") > -1){
           rappRosServices.push(platformActiveServices[i])
@@ -120,6 +178,9 @@ function loopInf(){
     })
     ros.getNodes(function(data){
       platformActiveNodes = data;
+    })
+    ros.getTopics(function(data){
+      platformActiveTopics = data;
     })
 
     loopInf();
