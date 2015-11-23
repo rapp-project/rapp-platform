112ec79f52ded2374cc3a65bc80b9edabdfb28dc
Aris Thallas <aris.thallas@gmail.com>
2015-11-24 00:10:54 +0200

    Fixed license in knowrob_wrapper

--- rapp_knowrob_wrapper/include/knowrob_wrapper/knowrob_wrapper.h
+++ rapp_knowrob_wrapper/include/knowrob_wrapper/knowrob_wrapper.h
@@ -1,29 +1,22 @@
-/**
-MIT License (MIT)
+/******************************************************************************
+Copyright 2015 RAPP
 
-Copyright (c) <2014> <Rapp Project EU>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   http://www.apache.org/licenses/LICENSE-2.0
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+  Author: Athanassios Kintsakis
+  contact: akintsakis@issel.ee.auth.gr
 
-Author: Athanassios Kintsakis
-contact: akintsakis@issel.ee.auth.gr
-**/
+******************************************************************************/
 
 #include <string>
 #include <iostream>
@@ -50,44 +43,44 @@ contact: akintsakis@issel.ee.auth.gr
 class KnowrobWrapper
 {
   private:
-    ros::NodeHandle nh_;  
-    json_prolog::Prolog pl;    
+    ros::NodeHandle nh_;
+    json_prolog::Prolog pl;
     ros::ServiceClient mysql_write_client;
     ros::ServiceClient mysql_fetch_client;
     ros::ServiceClient mysql_update_client;
-  
+
   public:
-  
+
     KnowrobWrapper(ros::NodeHandle nh);
     std::string get_ontology_alias(std::string user_id);
     std::string create_ontology_alias_for_new_user(std::string user_id);
-    
-    
+
+
     rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Response  subclassesOfQuery(rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Request req);
     rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Response  superclassesOfQuery(rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Request req);
-        
+
     rapp_platform_ros_communications::ontologyIsSubSuperClassOfSrv::Response  isSubSuperclassOfQuery(rapp_platform_ros_communications::ontologyIsSubSuperClassOfSrv::Request req);
         //std::vector<std::string> createInstanceQuery(std::string caller_arguments);
     rapp_platform_ros_communications::createInstanceSrv::Response createInstanceQuery(rapp_platform_ros_communications::createInstanceSrv::Request req);
     rapp_platform_ros_communications::ontologyLoadDumpSrv::Response dumpOntologyQuery(rapp_platform_ros_communications::ontologyLoadDumpSrv::Request req);
-    rapp_platform_ros_communications::ontologyLoadDumpSrv::Response loadOntologyQuery(rapp_platform_ros_communications::ontologyLoadDumpSrv::Request req);  
+    rapp_platform_ros_communications::ontologyLoadDumpSrv::Response loadOntologyQuery(rapp_platform_ros_communications::ontologyLoadDumpSrv::Request req);
     rapp_platform_ros_communications::assertRetractAttributeSrv::Response assertAttributeValue(rapp_platform_ros_communications::assertRetractAttributeSrv::Request req);
-    
+
     rapp_platform_ros_communications::returnUserInstancesOfClassSrv::Response user_instances_of_class(rapp_platform_ros_communications::returnUserInstancesOfClassSrv::Request req);
 
     rapp_platform_ros_communications::createOntologyAliasSrv::Response create_ontology_alias(rapp_platform_ros_communications::createOntologyAliasSrv::Request req);
-    
+
     rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Response user_performance_cognitve_tests(rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Request req);
-    
+
     rapp_platform_ros_communications::createCognitiveExerciseTestSrv::Response create_cognitve_tests(rapp_platform_ros_communications::createCognitiveExerciseTestSrv::Request req);
-    
+
     rapp_platform_ros_communications::cognitiveTestsOfTypeSrv::Response cognitive_tests_of_type(rapp_platform_ros_communications::cognitiveTestsOfTypeSrv::Request req);
-    
+
     rapp_platform_ros_communications::recordUserPerformanceCognitiveTestsSrv::Response record_user_cognitive_tests_performance(rapp_platform_ros_communications::recordUserPerformanceCognitiveTestsSrv::Request req);
-    
+
     rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Response clear_user_cognitive_tests_performance_records(rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Request req);
-    
-    
+
+
     //std::vector<std::string> userInstancesFromClassQuery(std::string ontology_class);
     //std::vector<std::string> checkIfClassExists(std::string classValue);
     //
--- rapp_knowrob_wrapper/include/knowrob_wrapper/knowrob_wrapper_communications.h
+++ rapp_knowrob_wrapper/include/knowrob_wrapper/knowrob_wrapper_communications.h
@@ -1,29 +1,22 @@
-/**
-MIT License (MIT)
+/******************************************************************************
+Copyright 2015 RAPP
 
-Copyright (c) <2014> <Rapp Project EU>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+   http://www.apache.org/licenses/LICENSE-2.0
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+  Author: Athanassios Kintsakis
+  contact: akintsakis@issel.ee.auth.gr
 
-Author: Athanassios Kintsakis
-contact: akintsakis@issel.ee.auth.gr
-**/
+******************************************************************************/
 
 #include <knowrob_wrapper/knowrob_wrapper.h>
 //#include <rapp_platform_ros_communications/OntologySimpleQuerySrv.h>
@@ -34,60 +27,60 @@ contact: akintsakis@issel.ee.auth.gr
 class KnowrobWrapperCommunications
 {
   private:
-    ros::NodeHandle nh_;    
+    ros::NodeHandle nh_;
     KnowrobWrapper knowrob_wrapper;
 
-    
-    ros::ServiceServer subclasses_of_service_;    
+
+    ros::ServiceServer subclasses_of_service_;
     std::string subclasses_of_service_topic_;
-    
-    ros::ServiceServer superclasses_of_service_;    
-    std::string superclasses_of_service_topic_;    
-    
-    ros::ServiceServer is_subsuperclass_of_service_;    
+
+    ros::ServiceServer superclasses_of_service_;
+    std::string superclasses_of_service_topic_;
+
+    ros::ServiceServer is_subsuperclass_of_service_;
     std::string is_subsuperclass_of_service_topic_;
-    
-    ros::ServiceServer createInstanceService_;    
+
+    ros::ServiceServer createInstanceService_;
     std::string createInstanceServiceTopic_;
-    
-    ros::ServiceServer dumpOntologyService_;    
+
+    ros::ServiceServer dumpOntologyService_;
     std::string dumpOntologyServiceTopic_;
-    
-    ros::ServiceServer loadOntologyService_;    
+
+    ros::ServiceServer loadOntologyService_;
     std::string loadOntologyServiceTopic_;
-    
-    ros::ServiceServer user_instances_of_class_service_;    
+
+    ros::ServiceServer user_instances_of_class_service_;
     std::string user_instances_of_class_topic_;
-    
-    ros::ServiceServer create_ontology_alias_service_;    
+
+    ros::ServiceServer create_ontology_alias_service_;
     std::string create_ontology_alias_topic_;
-    
-    ros::ServiceServer user_performance_cognitve_tests_service_;    
+
+    ros::ServiceServer user_performance_cognitve_tests_service_;
     std::string user_performance_cognitve_tests_topic_;
-    
-    ros::ServiceServer create_cognitve_tests_service_;    
+
+    ros::ServiceServer create_cognitve_tests_service_;
     std::string create_cognitve_tests_topic_;
-    
-    ros::ServiceServer cognitive_tests_of_type_service_;    
+
+    ros::ServiceServer cognitive_tests_of_type_service_;
     std::string cognitive_tests_of_type_topic_;
-    
-    ros::ServiceServer record_user_cognitive_tests_performance_service_;    
+
+    ros::ServiceServer record_user_cognitive_tests_performance_service_;
     std::string record_user_cognitive_tests_performance_topic_;
-   
-    ros::ServiceServer clear_user_cognitive_tests_performance_records_service_;    
+
+    ros::ServiceServer clear_user_cognitive_tests_performance_records_service_;
     std::string clear_user_cognitive_tests_performance_records_topic_;
-    
-    
-        
-    //ros::ServiceServer userInstancesFromClassService_;    
-    //std::string userInstancesFromClassServiceTopic_;   
-    
-    //ros::ServiceServer assignAttributeValueService_;    
-    //std::string assignAttributeValueServiceTopic_;       
-    //ros::ServiceServer instanceFromClassService_;    
+
+
+
+    //ros::ServiceServer userInstancesFromClassService_;
+    //std::string userInstancesFromClassServiceTopic_;
+
+    //ros::ServiceServer assignAttributeValueService_;
+    //std::string assignAttributeValueServiceTopic_;
+    //ros::ServiceServer instanceFromClassService_;
     //std::string instanceFromClassServiceTopic_;
-    
-    
+
+
 
   public:
 
@@ -96,32 +89,32 @@ class KnowrobWrapperCommunications
     bool subclassesOfCallback(
       rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Request& req,
       rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Response& res);
-      
+
     bool superclassesOfCallback(
       rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Request& req,
       rapp_platform_ros_communications::ontologySubSuperClassesOfSrv::Response& res);
-      
-      
+
+
     bool isSubSuperclassOfCallback(
       rapp_platform_ros_communications::ontologyIsSubSuperClassOfSrv::Request& req,
       rapp_platform_ros_communications::ontologyIsSubSuperClassOfSrv::Response& res);
-            
+
     bool createInstanceCallback(
       rapp_platform_ros_communications::createInstanceSrv::Request& req,
       rapp_platform_ros_communications::createInstanceSrv::Response& res);
-      
+
     bool dumpOntologyCallback(
       rapp_platform_ros_communications::ontologyLoadDumpSrv::Request& req,
       rapp_platform_ros_communications::ontologyLoadDumpSrv::Response& res);
-      
+
     bool loadOntologyCallback(
       rapp_platform_ros_communications::ontologyLoadDumpSrv::Request& req,
       rapp_platform_ros_communications::ontologyLoadDumpSrv::Response& res);
-      
+
     bool user_instances_of_class_callback(
       rapp_platform_ros_communications::returnUserInstancesOfClassSrv::Request& req,
       rapp_platform_ros_communications::returnUserInstancesOfClassSrv::Response& res);
-      
+
     bool create_ontology_alias_callback(
       rapp_platform_ros_communications::createOntologyAliasSrv::Request& req,
       rapp_platform_ros_communications::createOntologyAliasSrv::Response& res);
@@ -129,7 +122,7 @@ class KnowrobWrapperCommunications
     bool user_performance_cognitve_tests_callback(
       rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Request& req,
       rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Response& res);
-     
+
     bool create_cognitve_tests_callback(
       rapp_platform_ros_communications::createCognitiveExerciseTestSrv::Request& req,
       rapp_platform_ros_communications::createCognitiveExerciseTestSrv::Response& res);
@@ -137,7 +130,7 @@ class KnowrobWrapperCommunications
     bool cognitive_tests_of_type_callback(
       rapp_platform_ros_communications::cognitiveTestsOfTypeSrv::Request& req,
       rapp_platform_ros_communications::cognitiveTestsOfTypeSrv::Response& res);
-      
+
     bool record_user_cognitive_tests_performance_callback(
       rapp_platform_ros_communications::recordUserPerformanceCognitiveTestsSrv::Request& req,
       rapp_platform_ros_communications::recordUserPerformanceCognitiveTestsSrv::Response& res);
@@ -147,14 +140,14 @@ class KnowrobWrapperCommunications
       rapp_platform_ros_communications::userPerformanceCognitveTestsSrv::Response& res);
 
 
-      
+
     //bool userInstancesFromClassCallback(
       //rapp_platform_ros_communications::OntologySimpleQuerySrv::Request& req,
       //rapp_platform_ros_communications::OntologySimpleQuerySrv::Response& res);
-      
+
     //bool assignAttributeValueCallback(
       //rapp_platform_ros_communications::OntologySimpleQuerySrv::Request& req,
       //rapp_platform_ros_communications::OntologySimpleQuerySrv::Response& res);
-      
-      
+
+
 };
