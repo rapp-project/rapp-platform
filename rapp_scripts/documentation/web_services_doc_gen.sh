97588be54ad1ce628bb0525a3cb9bf7b3fa56427
Aris Thallas <aris.thallas@gmail.com>
2015-11-27 15:07:20 +0200

    Changed folder paths

--- rapp_scripts/documentation/web_services_doc_gen.sh
+++ rapp_scripts/documentation/web_services_doc_gen.sh
@@ -4,12 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 WEB_SRV_DIR="${DIR}/../../rapp_web_services"
 
-RAPP_PLATFORM_FILES_DIR="${HOME}/rapp_platform_files"
-DOC_DEST="${RAPP_PLATFORM_FILES_DIR}/rapp_web_services_doc"
+RAPP_PLATFORM_FILES_DIR="${HOME}/rapp_platform_files/documentation"
+DOC_DEST="${RAPP_PLATFORM_FILES_DIR}/platform_web_services"
 
 cd ${WEB_SRV_DIR}
-make doc
+make doc &> /dev/null
 mkdir -p ${DOC_DEST}
-cp -avr doc/* ${DOC_DEST}/
-make clean
+cp -avr doc/* ${DOC_DEST}/ &> /dev/null
+make clean &> /dev/null
 
