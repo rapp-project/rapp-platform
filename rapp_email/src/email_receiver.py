8358a77c973942608c7fc6faf6aca7aaebdd5f14
Aris Thallas <aris.thallas@gmail.com>
2015-12-15 17:00:06 +0200

    Added MailMsg and fixed ReceiveEmailSrv accordingly. Work on mail_receiver.
    [ci skip]

--- rapp_email/src/email_receiver.py
+++ rapp_email/src/email_receiver.py
@@ -22,6 +22,8 @@ import os
 import imaplib
 import email
 
+import rospy
+
 from rapp_platform_ros_communications.srv import (
   ReceiveEmailSrv,
   ReceiveEmailSrvResponse
@@ -31,4 +33,70 @@ from rapp_platform_ros_communications.srv import (
 class EmailReceiver(object):
 
   def __init__(self):
+    sendSrvTopic = rospy.get_param("rapp_email_send_topic")
+    s = rospy.Service(sendSrvTopic, ReceiveEmailSrv, \
+                      self.receiveEmailSrvCallback)
+
+
+  def receiveEmailSrvCallback(self, req):
+
+    resp = ReceiveEmailSrvResponse()
+
+    userEmail, emailPass = self._getEmailAndPass(req.username)
+
+    try:
+      imapConn = self._connectImap(userEmail, emailPass)
+    except imaplib.IMAP4.error, err:
+      rospy.logerr( str(err) )
+      resp.status = -1
+      return resp
+
+
+
+
+
+
+
+  ## @brief Fetches user's email and email's password from db
+  #
+  # @param username [string] The rapp user username
+  #
+  # @return email    [string] The user's email
+  # @return password [string] The user's password
+  def _getEmailAndPass(self, username):
     pass
+
+  ## @brief Create an IMAP connection to the server.
+  #
+  # Initiates an IMAP connection according to the user's email and logins to
+  # the server using user's credentials
+  #
+  # @param email    [string] The user's email
+  # @param password [string] The user's password
+  #
+  # @return imap [imaplib::IMAP4_SSL] The connection
+  def _connectImap(self, email, password):
+
+    imapServer = 'imap.' + email.split('@')[2]
+    try:
+      imap = imaplib.IMAP4_SSL( imapServer )
+    except imaplib.IMAP4.error:
+      rospy.logerr("Could not establish a connection to the requested IMAP" + \
+          ' server: ' + imapServer )
+      raise
+
+    try:
+      imap = imaplib.IMAP4_SSL( imapServer )
+    except imaplib.IMAP4.error:
+      rospy.logerr("Could not login to the requested IMAP" + \
+          ' server: ' + imapServer )
+      raise
+
+    return imap
+
+
+
+if __name__ == '__main__':
+    rospy.logerr('Implements server for EmailReceiverSrv. Not supposed to' + \
+        ' be called directly')
+    exit(-1)
