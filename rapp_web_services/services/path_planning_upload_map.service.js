167bf79dcc34a228862ef1778a9f5a24a5e2326d
dudekw <wojciechsbox@gmail.com>
2016-01-30 17:46:57 +0100

    fix path_planning_upload_map service documentation

--- rapp_web_services/services/path_planning_upload_map.service.js
+++ rapp_web_services/services/path_planning_upload_map.service.js
@@ -22,7 +22,7 @@
 /***
  * @fileOverview
  *
- * [Path-planning] RAPP Platform front-end web service.
+ * [Path-planning-upload-map] RAPP Platform front-end web service.
  *
  *  @author Wojciech Dudek
  *  @mail wojciechsbox@gmail.com
@@ -57,15 +57,6 @@ var __servicesCacheDir = Fs.resolvePath( pathsEnv.cache_dir_services );
 var __serverCacheDir = Fs.resolvePath( pathsEnv.cache_dir_server );
 /* ----------------------------------------------------------------------- */
 
-//var rosSrvName = srvEnv[__hopServiceName].ros_srv_name;
-
-// Initiate connection to rosbridge_websocket_server
-//var ros = new ROS({hostname: '', port: '', reconnect: true, onconnection:
-//  function(){
-    // .
- // }
-//});
-
 /*----------------< Random String Generator configurations >---------------*/
 var stringLength = 5;
 var randStrGen = new RandStringGen( stringLength );
@@ -84,21 +75,22 @@ var colors = {
   clear:    String.fromCharCode(0x1B) + '[0m'
 };
 
-
-// Register communication interface with the master-process
-//register_master_interface();
-
-
-
 /**
- *  [Face-Detection] RAPP Platform front-end web service.
- *  <p> Serves requests for face_detection on given input image frame.</p>
+ *  [Path-planning-upload-map] RAPP Platform front-end web service.
+ *  <p> Serves requests for path_planning_upload_map on given input: .png file, .yaml file, user name, map name.</p>
  *
- *  @function face_detection
+ *  @function path_planning_upload_map
  *
  *  @param {Object} args - Service input arguments (object literal).
- *  @param {String} args.file_uri - System uri path of transfered (client)
- *    file, as declared in multipart/form-data post field. The file_uri is
+ *  @param {String} args.PNGfile_uri - System uri path of transfered (client)
+ *    file, as declared in multipart/form-data post field. The PNGfile_uri is
+ *    handled and forwared to this service, as input argument,
+ *    by the HOP front-end server.
+ *    Clients are responsible to declare this field in the multipart/form-data
+ *    post field.
+ *
+ *  @param {String} args.YAMLfile_uri - System uri path of transfered (client)
+ *    file, as declared in multipart/form-data post field. The YAMLfile_uri is
  *    handled and forwared to this service, as input argument,
  *    by the HOP front-end server.
  *    Clients are responsible to declare this field in the multipart/form-data
@@ -106,16 +98,15 @@ var colors = {
  *
  *  @returns {Object} response - JSON HTTPResponse Object.
  *    Asynchronous HTTP Response.
- *  @returns {Array} response.faces - An array of face-objects.
+ *  @returns {bool} response.success - Status of performed operations.
  *  @returns {String} response.error - Error message string to be filled
  *    when an error has been occured during service call.
  */
 service path_planning_upload_map({PNGfile_uri: '', YAMLfile_uri: '', user_name: '', map_name: ''})
-/*, resolution: 0.02, origin: [0,0, 0], negate: 0, occupied_thresh: 0.65, free_thresh: 0.196)*/
 {
 
   /***
-   *  For security reasons, if file_uri is not defined under the
+   *  For security reasons, if PNGfile_uri or YAMLfile_uri is not defined under the
    *  server_cache_dir do not operate. HOP server stores the files under the
    *  __serverCacheDir directory.
    */
@@ -142,322 +133,23 @@ service path_planning_upload_map({PNGfile_uri: '', YAMLfile_uri: '', user_name: 
     return hop.HTTPResponseJson(response);
   }
 
-var cpPNGFilePath = "/home/rapp/rapp_platform_files/maps/"+user_name+"/"+map_name+".png";
-var cpYAMLFilePath = "/home/rapp/rapp_platform_files/maps/"+user_name+"/"+map_name+".yaml";
-if(Fs.isDirectory("/home/rapp/rapp_platform_files/maps/"+user_name)){
-}else{
-Fs.createDir("/home/rapp/rapp_platform_files/maps/"+user_name);
-}
-Fs.renameFile(PNGfile_uri, cpPNGFilePath);
-Fs.renameFile(YAMLfile_uri, cpYAMLFilePath);
+  var cpPNGFilePath = "/home/rapp/rapp_platform_files/maps/"+user_name+"/"+map_name+".png";
+  var cpYAMLFilePath = "/home/rapp/rapp_platform_files/maps/"+user_name+"/"+map_name+".yaml";
+  //
+  // create user directory if one does not exist.
+  //
+  if(!Fs.isDirectory("/home/rapp/rapp_platform_files/maps/"+user_name))
+      Fs.createDir("/home/rapp/rapp_platform_files/maps/"+user_name);
+  //
+  // coppy .png and .yaml files from the server_cache_dir
+  //
+  Fs.renameFile(PNGfile_uri, cpPNGFilePath);
+  Fs.renameFile(YAMLfile_uri, cpYAMLFilePath);
 
- // Fs.writeFileSync("/home/rapp/rapp_platform_files/maps/"+user_name+"/"+map_name+".png",PNGfile_uri );
- // Fs.writeFileSync("~/rapp_platform_files/maps/"+user_name+"/"+map_name+".yaml",YAMLfile_uri );
-    var response = {success: true, error: ''};
+  var response = {success: true, error: ''};
  return hop.HTTPResponseJson(response);
-
-
-
-  /* ----------------------------------------------------------------------- 
-
-  // Assign a unique identification key for this service request.
-  var unqCallId = randStrGen.createUnique();
-
-  var startT = new Date().getTime();
-  var execTime = 0;
-
-  postMessage( craft_slaveMaster_msg('log', 'client-request {' + rosSrvName +
-    '}') );
-  var logMsg = 'Image stored at [' + file_uri + ']';
-  postMessage( craft_slaveMaster_msg('log', logMsg) );
-*/
-  /* --< Perform renaming on the reived file. Add uniqueId value> --- 
-  var fileUrl = file_uri.split('/');
-  var fileName = fileUrl[fileUrl.length -1];
-
-  var cpFilePath = __servicesCacheDir + fileName.split('.')[0] + '-'  +
-    unqCallId + '.' + fileName.split('.')[1];
-  cpFilePath = Fs.resolvePath(cpFilePath);*/
-  /* ---------------------------------------------------------------- */
-
-
-  /* --------------------- Handle transferred file ------------------------- 
-  if (Fs.renameFile(file_uri, cpFilePath) === false)
-  {
-    //could not rename file. Probably cannot access the file. Return to client!
-    var logMsg = 'Failed to rename file: [' + file_uri + '] --> [' +
-      cpFilePath + ']';
-
-    postMessage( craft_slaveMaster_msg('log', logMsg) );
-    Fs.rmFile(file_uri);
-    randStrGen.removeCached(unqCallId);
-    var response = craft_error_response();
-    return hop.HTTPResponseJson(response);
-  }
-  logMsg = 'Created copy of file ' + file_uri + ' at ' + cpFilePath;
-  postMessage( craft_slaveMaster_msg('log', logMsg) );*/
-  /*-------------------------------------------------------------------------
-
-  // Workaround for bool and hop
-  if (fast == 'True' || fast == 'true'){ fast = true; }
-  //else { fast_input = false;}
-  if (fast == 'False' || fast == 'false'){ fast = false; }*/
-
-  /***
-   * Asynchronous http response
-   *//*
-  return hop.HTTPResponseAsync(
-    function( sendResponse ) {
-
-      /***
-       *  Status flags.
-       *//*
-      var respFlag = false;
-      var retClientFlag = false;
-      var wsError = false;
-      var retries = 0;
-      /* --------------------------------------------------- */
-
-      // Fill Ros Service request msg parameters here.
-     /* var args = {
-        imageFilename: cpFilePath,
-        fast: fast
-      };
-
-
-      /***
-       * Declare the service response callback here!!
-       * This callback function will be passed into the rosbridge service
-       * controller and will be called when a response from rosbridge
-       * websocket server arrives.
-       */
-    /*  function callback(data){
-        respFlag = true;
-        if( retClientFlag ) { return; }
-        // Remove this call id from random string generator cache.
-        randStrGen.removeCached( unqCallId );
-        // Remove cached file. Release resources.
-        Fs.rmFile(cpFilePath);
-        //console.log(data);
-        // Craft client response using ros service ws response.
-        var response = craft_response( data );
-        // Asynchronous response to client.
-        sendResponse( hop.HTTPResponseJson(response) );
-        retClientFlag = true;
-      }
-
-      /***
-       * Declare the onerror callback.
-       * The onerror callack function will be called by the service
-       * controller as soon as an error occures, on service request.
-       */
-    /*  function onerror(e){
-        respFlag = true;
-        if( retClientFlag ) { return; }
-        // Remove this call id from random string generator cache.
-        randStrGen.removeCached( unqCallId );
-        // Remove cached file. Release resources.
-        Fs.rmFile(cpFilePath);
-        // craft error response
-        var response = craft_error_response();
-        // Asynchronous response to client.
-        sendResponse( hop.HTTPResponseJson(response) );
-        retClientFlag = true;
-      }
-
-
-      // Invoke ROS-Service request.
-      ros.callService(rosSrvName, args,
-        {success: callback, fail: onerror});
-
-      /***
-       * Set Timeout wrapping function.
-       * Polling in defined time-cycle. Catch timeout connections etc...
-       */
-    /*  function asyncWrap(){
-        setTimeout( function(){
-
-         /***
-          * If received message from rosbridge websocket server or an error
-          * on websocket connection, stop timeout events.
-          */
-      /*    if ( respFlag || wsError || retClientFlag ) { return; }
-
-          retries += 1;
-
-          var logMsg = 'Reached rosbridge response timeout' + '---> [' +
-            timeout.toString() + '] ms ... Reconnecting to rosbridge.' +
-            'Retry-' + retries;
-          postMessage( craft_slaveMaster_msg('log', logMsg) );
-
-          /***
-           * Fail. Did not receive message from rosbridge.
-           * Return to client.
-           */
-      /*    if ( retries >= maxTries )
-          {
-            logMsg = 'Reached max_retries [' + maxTries + ']' +
-              ' Could not receive response from rosbridge...';
-            postMessage( craft_slaveMaster_msg('log', logMsg) );
-
-            // Remove cached file. Release resources.
-            Fs.rmFile(cpFilePath);
-
-            execTime = new Date().getTime() - startT;
-            postMessage( craft_slaveMaster_msg('execTime', execTime) );
-
-            var response = craft_error_response();
-            sendResponse( hop.HTTPResponseJson(response));
-            retClientFlag = true;
-            return;
-          }
-          /*--------------------------------------------------------*/
-      /*    asyncWrap();
-
-        }, timeout);
-      }
-      asyncWrap();
-      /*=================================================================*/
-  /*  }, this );*/
 }
 
-
-
-
-/***
- * Crafts response object.
- *
- *  @param {Object} rosbridge_msg - Return message from rosbridge
- *
- *  @returns {Object} response - Response Object.
- *  @returns {Array} response.faces - An array of face-objects.
- *  @returns {String} response.error - Error message string to be filled
- *    when an error has been occured during service call.
- */
-function craft_response(rosbridge_msg)
-{
-  var faces_up_left = rosbridge_msg.faces_up_left;
-  var faces_down_right = rosbridge_msg.faces_down_right;
-  var error = rosbridge_msg.error;
-  var numFaces = faces_up_left.length;
-
-  var logMsg = 'Returning to client';
-
-  var response = {
-    faces: [],
-    error: ''
-  };
-
-  for (var ii = 0; ii < numFaces; ii++)
-  {
-    /***
-     * @namespace face
-     * @property up_left_point - Face bounding box, up-left-point
-     */
-    var face = {
-      up_left_point: {x: 0, y:0},
-      down_right_point: {x: 0, y: 0}
-    };
-
-    face.up_left_point.x = faces_up_left[ii].point.x;
-    face.up_left_point.y = faces_up_left[ii].point.y;
-    face.down_right_point.x = faces_down_right[ii].point.x;
-    face.down_right_point.y = faces_down_right[ii].point.y;
-    response.faces.push( face );
-  }
-
-  response.error = error;
-
-  if (error !== '')
-  {
-    logMsg += ' ROS service [' + rosSrvName + '] error' +
-      ' ---> ' + error;
-  }
-  else
-  {
-    logMsg += ' ROS service [' + rosSrvName + '] returned with success';
-  }
-  postMessage( craft_slaveMaster_msg('log', logMsg) );
-
-  return response;
-}
-
-
-/***
- *  Craft service error response object. Used to return to client when an
- *  error has been occured, while processing client request.
- */
-function craft_error_response()
-{
-  var errorMsg = 'RAPP Platform Failure';
-
-  var response = {
-    faces: [],
-    error: errorMsg
-  };
-
-  var logMsg = 'Return to client with error --> ' + errorMsg;
-  postMessage( craft_slaveMaster_msg('log', logMsg) );
-
-  return response;
-}
-
-
-/***
- *  Register interface with the main hopjs process. After registration
- *  this worker service can communicate with the main hopjs process through
- *  websockets.
- *
- *  The global scoped postMessage is used in order to send messages to the main
- *  process.
- *  Furthermore, the global scoped onmessage callback function declares the
- *  handler for incoming messages from the hopjs main process.
- *
- *  Currently log messages are handled by the main process.
- */
-function register_master_interface()
-{
-  // Register onexit callback function
-  onexit = function(e){
-    console.log("Service [%s] exiting...", __hopServiceName);
-    var logMsg = "Received termination command. Exiting.";
-    postMessage( craft_slaveMaster_msg('log', logMsg) );
-  };
-
-  // Register onmessage callback function
-  onmessage = function(msg){
-    if (__DEBUG__)
-    {
-      console.log("Service [%s] received message from master process",
-        __hopServiceName);
-      console.log("Msg -->", msg.data);
-    }
-
-    var logMsg = 'Received message from master process --> [' +
-      msg.data + ']';
-    postMessage( craft_slaveMaster_msg('log', logMsg) );
-
-    var cmd = msg.data.cmdId;
-    var data = msg.data.data;
-    switch (cmd)
-    {
-      case 2055:  // Set worker ID
-        __hopServiceId = data;
-        break;
-      default:
-        break;
-    }
-  };
-
-  // On initialization inform master and append to log file
-  var logMsg = "Initiated worker";
-  postMessage( craft_slaveMaster_msg('log', logMsg) );
-}
-
-
-/***
- *  Returns master-process comm msg literal.
- */
 function craft_slaveMaster_msg(msgId, msg)
 {
   var _msg = {
