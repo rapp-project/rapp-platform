552613871d68d5f08477be166f6d085439b7e123
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-02-26 20:42:32 +0200

    Register speech_detection_sphinx4 services

---
+++ rapp_web_services/services/speech_detection_sphinx4/iface_obj.js
@@ -0,0 +1,67 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+var client_res = function( ){
+  var obj = {
+    words: [],
+    alternatives: [],
+    error: ''
+  };
+  return obj;
+};
+
+
+var client_req = function( ){
+  var obj = {
+    file_uri: '',
+    audio_source: '',
+    user: '',
+    language: '',
+    words: [],
+    sentences: [],
+    grammar: []
+  };
+  return obj;
+};
+
+
+var ros_req = function( ){
+  var obj = {
+    path: '',
+    audio_source: '',
+    user: '',
+    language: '',
+    words: [],
+    sentences: [],
+    grammar: []
+  };
+  return obj;
+};
+
+
+var ros_res = function(){
+
+};
+
+exports.client_res = client_res;
+exports.client_req = client_req;
+exports.ros_req = ros_req;
+exports.ros_res = ros_res;
