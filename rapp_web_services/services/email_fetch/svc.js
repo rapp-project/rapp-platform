06f619614d48a464fcd15cd2818cfebccafb6d08
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-02-28 21:27:12 +0200

    New web service - email_fetch

---
+++ rapp_web_services/services/email_fetch/svc.js
@@ -0,0 +1,277 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+/***
+ * @fileOverview
+ *
+ * [Face-Detection] RAPP Platform front-end web service.
+ *
+ *  @author Konstantinos Panayiotou
+ *  @copyright Rapp Project EU 2015
+ */
+
+
+var hop = require('hop');
+var path = require('path');
+var util = require('util');
+
+var PKG_DIR = ENV.PATHS.PKG_DIR;
+var INCLUDE_DIR = ENV.PATHS.INCLUDE_DIR;
+
+var svcUtils = require(path.join(INCLUDE_DIR, 'common',
+    'svc_utils.js'));
+
+var RandStringGen = require ( path.join(INCLUDE_DIR, 'common',
+    'randStringGen.js') );
+
+var ROS = require( path.join(INCLUDE_DIR, 'rosbridge', 'src',
+    'Rosbridge.js') );
+
+var interfaces = require( path.join(__dirname, 'iface_obj.js') );
+
+/* ------------< Load parameters >-------------*/
+var svcParams = ENV.SERVICES.email_fetch;
+var SERVICE_NAME = svcParams.name;
+var rosSrvName = svcParams.ros_srv_name;
+
+var SERVICES_CACHE_DIR = ENV.PATHS.SERVICES_CACHE_DIR;
+var SERVER_CACHE_DIR = ENV.PATHS.SERVER_CACHE_DIR;
+/* ----------------------------------------------------------------------- */
+
+// Instantiate interface to rosbridge-websocket-server
+var ros = new ROS({hostname: ENV.ROSBRIDGE.HOSTNAME, port: ENV.ROSBRIDGE.PORT,
+  reconnect: true, onconnection: function(){
+    // .
+  }
+});
+
+/*----------------< Random String Generator configurations >---------------*/
+var stringLength = 5;
+var randStrGen = new RandStringGen( stringLength );
+/* ----------------------------------------------------------------------- */
+
+/* ------< Set timer values for websocket communication to rosbridge> ----- */
+var timeout = svcParams.timeout; // ms
+var maxTries = svcParams.retries;
+/* ----------------------------------------------------------------------- */
+
+
+/**
+ *  [Face-Detection] RAPP Platform front-end web service.
+ *  <p> Serves requests for face_detection on given input image frame.</p>
+ *
+ *  @function face_detection
+ *
+ *  @param {Object} args - Service input arguments (object literal).
+ *  @param {String} args.file_uri - System uri path of transfered (client)
+ *    file, as declared in multipart/form-data post field. The file_uri is
+ *    handled and forwared to this service, as input argument,
+ *    by the HOP front-end server.
+ *    Clients are responsible to declare this field in the multipart/form-data
+ *    post field.
+ *
+ *  @returns {Object} response - JSON HTTPResponse Object.
+ *    Asynchronous HTTP Response.
+ *  @returns {Array} response.faces - An array of face-objects.
+ *  @returns {String} response.error - Error message string to be filled
+ *    when an error has been occured during service call.
+ */
+function svcImpl ( kwargs )
+{
+  var req = new interfaces.client_req();
+  var response = new interfaces.client_res();
+  var error = '';
+
+  /* ------ Parse arguments ------ */
+  kwargs = kwargs || {};
+  for( var i in req ){
+    req[i] = (kwargs[i] !== undefined) ? kwargs[i] : req[i];
+  }
+  if( ! req.email ){
+    error = 'Empty \"email\" argument';
+    response.error = error;
+    return hop.HTTPResponseJson(response);
+  }
+  if( ! req.server ){
+    error = 'Empty \"server\" argument';
+    response.error = error;
+    return hop.HTTPResponseJson(response);
+  }
+  // To Number input arguments conversions
+  req.from_date = parseInt(req.from_date);
+  req.to_date = parseInt(req.to_date);
+  req.num_emails = parseInt(req.num_emails);
+
+
+  // Assign a unique identification key for this service request.
+  var unqCallId = randStrGen.createUnique();
+
+  var startT = new Date().getTime();
+  var execTime = 0;
+
+
+  /***
+   * Asynchronous http response
+   */
+  return hop.HTTPResponseAsync(
+    function( sendResponse ) {
+
+      /***
+       *  Status flags.
+       */
+      var respFlag = false;
+      var retClientFlag = false;
+      var wsError = false;
+      var retries = 0;
+      /* --------------------------------------------------- */
+
+      // Fill Ros Service request msg parameters here.
+      var rosSvcReq = new interfaces.ros_req();
+      rosSvcReq.email = req.email;
+      rosSvcReq.password = req.passwd;
+      rosSvcReq.server = req.server;
+      rosSvcReq.port = req.port;
+      rosSvcReq.requestedEmailStatus = req.email_status;
+      rosSvcReq.fromDate = req.from_date;
+      rosSvcReq.toDate = req.to_date;
+      rosSvcReq.numberOfEmails = req.num_emails;
+
+
+      /***
+       * Declare the service response callback here!!
+       * This callback function will be passed into the rosbridge service
+       * controller and will be called when a response from rosbridge
+       * websocket server arrives.
+       */
+      function callback(data){
+        respFlag = true;
+        if( retClientFlag ) { return; }
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        //console.log(data);
+        // Craft client response using ros service ws response.
+        var response = parseRosbridgeMsg( data );
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+        retClientFlag = true;
+      }
+
+      /***
+       * Declare the onerror callback.
+       * The onerror callack function will be called by the service
+       * controller as soon as an error occures, on service request.
+       */
+      function onerror(e){
+        respFlag = true;
+        if( retClientFlag ) { return; }
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        // craft error response
+        var response = new interfaces.client_res();
+        response.error = svcUtils.ERROR_MSG_DEFAULT;
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+        retClientFlag = true;
+      }
+
+
+      // Invoke ROS-Service request.
+      ros.callService(rosSrvName, rosSvcReq,
+        {success: callback, fail: onerror});
+
+      /***
+       * Set Timeout wrapping function.
+       * Polling in defined time-cycle. Catch timeout connections etc...
+       */
+      function asyncWrap(){
+        setTimeout( function(){
+
+         /***
+          * If received message from rosbridge websocket server or an error
+          * on websocket connection, stop timeout events.
+          */
+          if ( respFlag || wsError || retClientFlag ) { return; }
+
+          retries += 1;
+
+          var logMsg = 'Reached rosbridge response timeout' + '---> [' +
+            timeout.toString() + '] ms ... Reconnecting to rosbridge.' +
+            'Retry-' + retries;
+
+          /***
+           * Fail. Did not receive message from rosbridge.
+           * Return to client.
+           */
+          if ( retries >= maxTries )
+          {
+            logMsg = 'Reached max_retries [' + maxTries + ']' +
+              ' Could not receive response from rosbridge...';
+
+            execTime = new Date().getTime() - startT;
+
+            var response = new interfaces.client_res();
+            response.error = svcUtils.ERROR_MSG_DEFAULT;
+
+            sendResponse( hop.HTTPResponseJson(response));
+            retClientFlag = true;
+            return;
+          }
+          /*--------------------------------------------------------*/
+          asyncWrap();
+
+        }, timeout);
+      }
+      asyncWrap();
+      /*=================================================================*/
+    }, this );
+}
+
+
+/***
+ * Crafts response object.
+ *
+ *  @param {Object} rosbridge_msg - Return message from rosbridge
+ *
+ *  @returns {Object} response - Response Object.
+ *  @returns {Array} response.faces - An array of face-objects.
+ *  @returns {String} response.error - Error message string to be filled
+ *    when an error has been occured during service call.
+ */
+function parseRosbridgeMsg(rosbridge_msg)
+{
+  var success = rosbridge_msg.status;
+  var emails = rosbridge_msg.emails;
+  var logMsg = 'Returning to client';
+
+  var response = new interfaces.client_res();
+
+  if( success < 0 ){
+    // TODO What shall be returned to client????!!!!
+    response.error = "Failed to fetch emails";
+    return response;
+  }
+
+  console.log(emails);
+  return response;
+}
+
+
+registerSvc(svcImpl, svcParams);
