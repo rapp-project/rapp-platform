e530a3d65ebf70794381e1c08fbc2b2fcb8d8b8a
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-03-05 14:02:37 +0200

    New Web Service - weather_report_forecast

---
+++ rapp_web_services/services/weather_report_forecast/iface_obj.js
@@ -0,0 +1,75 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+var clientRes = function( ){
+  var obj = {
+    forecast: [],
+    error: ''
+  };
+  return obj;
+};
+
+
+var clientReq = function( ){
+  var obj = {
+    city: '',
+    weather_reporter: '',
+    metric: 0
+  };
+  return obj;
+};
+
+
+var rosReq = function( ){
+  var obj = {
+    city: '',
+    weather_reporter: '',
+    metric: 0
+  };
+  return obj;
+};
+
+
+var rosRes = function( ){
+  var obj = {
+    error: '',
+    forecast: []
+  };
+  return obj;
+};
+
+
+var forecastEntry = function( ){
+  var obj = {
+    high_temp: '',
+    low_temp: '',
+    description: '',
+    date: ''
+  };
+  return obj;
+};
+
+
+exports.client_res = clientRes;
+exports.client_req = clientReq;
+exports.ros_req = rosReq;
+exports.ros_res = rosRes;
+exports.forecast_entry = forecastEntry;
---
+++ rapp_web_services/services/weather_report_forecast/svc.js
@@ -0,0 +1,251 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+
+/***
+ * @fileOverview
+ *
+ * [Cognitive-get-history] RAPP Platform front-end web service.
+ *
+ *  @author Konstantinos Panayiotou
+ *  @copyright Rapp Project EU 2015
+ *
+ *  Basic Components (modules)
+ *
+ *      hop = require('hop').
+ *        Import and use hopjs functionalities.
+ *        For more information visit:
+ *            https://github.com/manuel-serrano/hop
+ *
+ *      RosBridgeJS.js
+ *        Use this module to achieve communication with ROS-framework.
+ *        This module integrates a service controller to connect to
+ *        the rosbridge-websocket-server.
+ *        For more information on rosbridge-websocket-server visit:
+ *            http://wiki.ros.org/rosbridge_suite
+ *
+ *        For more information on the RosBridgeJS module visit:
+ *            https://github.com/klpanagi/RosBridgeJS
+ *
+ *      RandStrGenerator.js
+ *        Random string generator class to generate cached unique
+ *        identity keys. Used to generate a unique id for each client
+ *        service request.
+ *
+ */
+
+var hop = require('hop');
+var path = require('path');
+var util = require('util');
+
+var PKG_DIR = ENV.PATHS.PKG_DIR;
+var INCLUDE_DIR = ENV.PATHS.INCLUDE_DIR;
+
+var svcUtils = require(path.join(INCLUDE_DIR, 'common',
+    'svc_utils.js'));
+
+var RandStringGen = require ( path.join(INCLUDE_DIR, 'common',
+    'randStringGen.js') );
+
+var ROS = require( path.join(INCLUDE_DIR, 'rosbridge', 'src',
+    'Rosbridge.js') );
+
+var interfaces = require( path.join(__dirname, 'iface_obj.js') );
+
+/* ------------< Load parameters >-------------*/
+var svcParams = ENV.SERVICES.weather_report_forecast;
+var rosSrvName = svcParams.ros_srv_name;
+/* ----------------------------------------------------------------------- */
+
+// Initiate communication with rosbridge-websocket-server
+var ros = new ROS({hostname: ENV.ROSBRIDGE.HOSTNAME, port: ENV.ROSBRIDGE.PORT,
+  reconnect: true, onconnection: function(){
+    // .
+  }
+});
+
+
+/*----------------< Random String Generator configurations >---------------*/
+var stringLength = 5;
+var randStrGen = new RandStringGen( stringLength );
+/* ----------------------------------------------------------------------- */
+
+/* ------< Set timer values for websocket communication to rosbridge> ----- */
+var timeout = svcParams.timeout; // ms
+var maxTries = svcParams.retries;
+/* ----------------------------------------------------------------------- */
+
+
+function svcImpl( kwargs )
+{
+  var req = new interfaces.client_req();
+  var response = new interfaces.client_res();
+  var error = '';
+
+  var clientHostname = this.socket.hostname;
+  var clientIpaddr = this.socket.hostAddr;
+
+
+  /* ------ Parse arguments ------ */
+  kwargs = kwargs || {};
+  for( var i in req ){
+    req[i] = (kwargs[i] !== undefined) ? kwargs[i] : req[i];
+  }
+  if( ! req.city ){
+    error = 'Empty \"city\" argument';
+    response.error = error;
+    return hop.HTTPResponseJson(response);
+  }
+
+  // Assign a unique identification key for this service request.
+  var unqCallId = randStrGen.createUnique();
+
+  /***
+   * Asynchronous http response.
+   */
+  return hop.HTTPResponseAsync(
+    function( sendResponse ) {
+
+      /***
+       *  Status flags.
+       *===========================*/
+      var respFlag = false;
+      var retClientFlag = false;
+      var wsError = false;
+      var retries = 0;
+      /*===========================*/
+
+      var rosSvcReq = new interfaces.ros_req();
+      rosSvcReq.city = req.city;
+      rosSvcReq.weather_reporter = req.weather_reporter;
+      rosSvcReq.metric = req.metric;
+
+
+      function callback(data){
+        respFlag = true;
+        if( retClientFlag ) { return; }
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        //console.log(data);
+        // Craft client response using ros service ws response.
+        var response = parseRosbridgeMsg( data );
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+        retClientFlag = true;
+      }
+
+      function onerror(e){
+        respFlag = true;
+        if( retClientFlag ) { return; }
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        // craft error response
+        var response = new interfaces.client_res();
+        response.error = svcUtils.ERROR_MSG_DEFAULT;
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+        retClientFlag = true;
+      }
+
+
+      ros.callService(rosSrvName, rosSvcReq,
+        {success: callback, fail: onerror});
+
+
+      function asyncWrap(){
+        setTimeout( function(){
+
+         /***
+          * If received message from rosbridge websocket server or an error
+          * on websocket connection, stop timeout events.
+          */
+          if ( respFlag || wsError || retClientFlag ) { return; }
+
+          retries += 1;
+
+          var logMsg = 'Reached rosbridge response timeout' + '---> [' +
+            timeout.toString() + '] ms ... Reconnecting to rosbridge.' +
+            'Retry-' + retries;
+
+          /***
+           * Fail. Did not receive message from rosbridge.
+           * Return to client.
+           */
+          if ( retries >= maxTries ){
+            randStrGen.removeCached( unqCallId );
+
+            logMsg = 'Reached max_retries [' + maxTries + ']' +
+              ' Could not receive response from rosbridge...';
+
+            var response = new interfaces.client_res();
+            response.error = svcUtils.ERROR_MSG_DEFAULT;
+
+            // Asynchronous client response.
+            sendResponse( hop.HTTPResponseJson(response));
+            retClientFlag = true;
+            return;
+          }
+          /*--------------------------------------------------------*/
+          asyncWrap();
+
+        }, timeout);
+      }
+      asyncWrap();
+      /*=====================================================================*/
+    }, this );
+}
+
+
+
+
+/***
+ * Craft response object.
+ *
+ *  @param {Object} rosbridge_msg - Return message from rosbridge
+ *  @returns {Object} response - Response Object.
+ *
+ */
+function parseRosbridgeMsg(rosbridge_msg)
+{
+  var response = new interfaces.client_res();
+  var forecast = rosbridge_msg.forecast;
+
+  var error = rosbridge_msg.error;
+
+  if( error ){
+    response.error = error;
+    return response;
+  }
+
+  for (var i in forecast){
+    var forecastEntry = new interfaces.forecast_entry();
+    forecastEntry.high_temp = forecast[i].high_temperature;
+    forecastEntry.low_temp = forecast[i].low_temperature;
+    forecastEntry.description = forecast[i].description;
+    forecastEntry.date = forecast[i].date;
+    response.forecast.push(forecastEntry);
+  }
+
+  return response;
+}
+
+
+registerSvc(svcImpl, svcParams);
