2ec17a835c4d6a8e15a67da4cfce9ccaf87b2a98
Maciej Stefańczyk <maciek.slon@gmail.com>
2016-03-09 19:02:10 +0100

    Add functional tests for door_check service

--- rapp_hazard_detection/tests/hazard_detection/functional_tests.py
+++ rapp_hazard_detection/tests/hazard_detection/functional_tests.py
@@ -25,15 +25,17 @@ import rospkg
 
 from rapp_platform_ros_communications.srv import (
   LightCheckRosSrv,
-  LightCheckRosSrvRequest
+  LightCheckRosSrvRequest,
+  DoorCheckRosSrv,
+  DoorCheckRosSrvRequest
   )
 
 
-class LightCheckFunc(unittest.TestCase):
-    """Handles the light checking functional tests
+class HazardDetectionFunc(unittest.TestCase):
+    """Handles the hazard detection functional tests
     """
     
-    ## Tests face detection with lamp turned on
+    ## Tests light detection with lamp turned on
     def test_lightOn(self):
         rospack = rospkg.RosPack()
         light_service = rospy.get_param("rapp_hazard_detection_light_check_topic")
@@ -46,7 +48,7 @@ class LightCheckFunc(unittest.TestCase):
         light_level = response.light_level
         self.assertGreater( light_level, 50 )
         
-    ## Tests face detection with lamp turned off
+    ## Tests light detection with lamp turned off
     def test_lightOff(self):
         rospack = rospkg.RosPack()
         light_service = rospy.get_param("rapp_hazard_detection_light_check_topic")
@@ -59,7 +61,7 @@ class LightCheckFunc(unittest.TestCase):
         light_level = response.light_level
         self.assertLess( light_level, 50 )
 
-    ## Stress test for face detection. 20 calls in a row
+    ## Stress test for light detection. 20 calls in a row
     def test_light_stress(self):
         rospack = rospkg.RosPack()
         light_service = rospy.get_param("rapp_hazard_detection_light_check_topic")
@@ -73,8 +75,8 @@ class LightCheckFunc(unittest.TestCase):
             light_level = response.light_level
             self.assertGreater( light_level, 50 )
 
-    ## Tests face detection with a non existent image. Should return 0 faces
-    def test_fileDoesNotExist(self):
+    ## Tests light detection with a non existent image. Should return -1
+    def test_light_fileDoesNotExist(self):
         rospack = rospkg.RosPack()
         light_service = rospy.get_param("rapp_hazard_detection_light_check_topic")
         rospy.wait_for_service(light_service)
@@ -86,8 +88,8 @@ class LightCheckFunc(unittest.TestCase):
         light_level = response.light_level
         self.assertEqual( light_level, -1 )
 
-    ## Tests face detection with an audio file. Should not crush an return 0 faces
-    def test_fileExistsButItAudio(self):
+    ## Tests light detection with an audio file. Should not crush an return -1
+    def test_light_fileExistsButItAudio(self):
         rospack = rospkg.RosPack()
         light_service = rospy.get_param("rapp_hazard_detection_light_check_topic")
         rospy.wait_for_service(light_service)
@@ -99,10 +101,77 @@ class LightCheckFunc(unittest.TestCase):
         light_level = response.light_level
         self.assertEqual( light_level, -1 )
 
+    ## Tests door detection with door opened
+    def test_doorOpen(self):
+        rospack = rospkg.RosPack()
+        door_service = rospy.get_param("rapp_hazard_detection_door_check_topic")
+        rospy.wait_for_service(door_service)
+        lc_service = rospy.ServiceProxy(door_service, DoorCheckRosSrv)
+        req = DoorCheckRosSrvRequest()
+        req.imageFilename = rospack.get_path('rapp_testing_tools') + \
+                '/test_data/hazard_detection_samples/door_2.png'
+        response = lc_service(req)
+        door_angle = response.door_angle
+        self.assertGreater( door_angle, 1 )
+        
+    ## Tests door detection with door closed
+    def test_doorClosed(self):
+        rospack = rospkg.RosPack()
+        door_service = rospy.get_param("rapp_hazard_detection_door_check_topic")
+        rospy.wait_for_service(door_service)
+        lc_service = rospy.ServiceProxy(door_service, DoorCheckRosSrv)
+        req = DoorCheckRosSrvRequest()
+        req.imageFilename = rospack.get_path('rapp_testing_tools') + \
+                '/test_data/hazard_detection_samples/door_4.png'
+        response = lc_service(req)
+        door_angle = response.door_angle
+        self.assertLess( door_angle, 2 )
+
+    ## Stress test for door detection. 20 calls in a row
+    def test_door_stress(self):
+        rospack = rospkg.RosPack()
+        door_service = rospy.get_param("rapp_hazard_detection_door_check_topic")
+        rospy.wait_for_service(door_service)
+        lc_service = rospy.ServiceProxy(door_service, DoorCheckRosSrv)
+        req = DoorCheckRosSrvRequest()
+        req.imageFilename = rospack.get_path('rapp_testing_tools') + \
+                '/test_data/hazard_detection_samples/door_1.png'
+        for i in range(0, 20):
+            response = lc_service(req)
+            door_angle = response.door_angle
+            self.assertGreater( door_angle, 0 )
+
+    ## Tests door detection with a non existent image. Should return -1
+    def test_door_fileDoesNotExist(self):
+        rospack = rospkg.RosPack()
+        door_service = rospy.get_param("rapp_hazard_detection_door_check_topic")
+        rospy.wait_for_service(door_service)
+        lc_service = rospy.ServiceProxy(door_service, DoorCheckRosSrv)
+        req = DoorCheckRosSrvRequest()
+        req.imageFilename = rospack.get_path('rapp_testing_tools') + \
+                '/test_data/not_existent_file.jpg'
+        response = lc_service(req)
+        door_angle = response.door_angle
+        self.assertEqual( door_angle, -1 )
+
+    ## Tests door detection with an audio file. Should not crush an return -1
+    def test_door_fileExistsButItAudio(self):
+        rospack = rospkg.RosPack()
+        door_service = rospy.get_param("rapp_hazard_detection_door_check_topic")
+        rospy.wait_for_service(door_service)
+        lc_service = rospy.ServiceProxy(door_service, DoorCheckRosSrv)
+        req = DoorCheckRosSrvRequest()
+        req.imageFilename = rospack.get_path('rapp_testing_tools') + \
+                '/test_data/silence_sample.wav'
+        response = lc_service(req)
+        door_angle = response.door_angle
+        self.assertEqual( door_angle, -1 )
+
+
 ## The main function. Initializes the functional tests
 if __name__ == '__main__':
     import rosunit
-    rosunit.unitrun(PKG, 'LightCheckFunc', LightCheckFunc)
+    rosunit.unitrun(PKG, 'HazardDetectionFunc', HazardDetectionFunc)
 
 
 
