7ddf3091f00ff60a971aff37b8d55dfb7b4e5d7e
thanos <athanassios.kintsakis@gmail.com>
2016-03-31 19:15:24 +0300

    updated rapp_application_authentication_manager, database_handler.py, untested

--- rapp_application_authentication_manager/src/rapp_application_authentication_manager/database_handler.py
+++ rapp_application_authentication_manager/src/rapp_application_authentication_manager/database_handler.py
@@ -15,9 +15,38 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-# Authors: Aris Thallas
+# Authors: Aris Thallas, Athanassios Kintsakis
 # contact: aris.thallas@{iti.gr, gmail.com}
 
+from rapp_platform_ros_communications.srv import (
+  getUserOntologyAliasSrv,
+  getUserOntologyAliasSrvResponse,
+  getUserOntologyAliasSrvRequest,
+  registerUserOntologyAliasSrv,
+  registerUserOntologyAliasSrvResponse,
+  registerUserOntologyAliasSrvRequest,
+  checkIfUserExistsSrv,
+  checkIfUserExistsSrvResponse,
+  checkIfUserExistsSrvRequest,
+  getUserLanguageSrv,
+  getUserLanguageSrvResponse,
+  getUserLanguageSrvRequest,
+  getUserPasswordSrv,
+  getUserPasswordSrvResponse,
+  getUserPasswordSrvRequest,
+  getUsernameAssociatedWithApplicationTokenSrv,
+  getUsernameAssociatedWithApplicationTokenSrvResponse,
+  getUsernameAssociatedWithApplicationTokenSrvRequest,
+  createNewPlatformUserSrv,
+  createNewPlatformUserSrvResponse,
+  createNewPlatformUserSrvRequest,
+  createNewApplicationTokenSrv,
+  createNewApplicationTokenSrvRequest,
+  createNewApplicationTokenSrvResponse,
+  checkActiveApplicationTokenSrv,
+  checkActiveApplicationTokenSrvRequest,
+  checkActiveApplicationTokenSrvResponse
+}
 
 class DatabaseHandler(object):
 
@@ -40,8 +69,18 @@ class DatabaseHandler(object):
     #
     # @return status [bool] True if username exists, false otherwise
     def username_exists(self, username):
-        #  TODO
-        pass
+      ros_service = rospy.get_param("rapp_mysql_wrapper_check_if_user_exists_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_check_if_user_exists_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, checkIfUserExistsSrv)
+      req = checkIfUserExistsSrvRequest()
+      req.username=username     
+      response = test_service(req)     
+      if(response.success):
+        return response.user_exists
+      else
+        return false
 
     ## Retrieve user's password from platform db
     #
@@ -49,12 +88,23 @@ class DatabaseHandler(object):
     #
     # @return password [string] Password associated with username
     def get_user_password(self, username):
-        #  TODO
-        pass
+      ros_service = rospy.get_param("rapp_mysql_wrapper_get_user_password_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_get_user_password_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, getUserPasswordSrv)
+      req = getUserPasswordSrvRequest()
+      req.username=username        
+      response = test_service(req)     
+      if(response.success):
+        return response.password
+      else:
+        #raise error
 
-    def verify_active_application_token(self, token):
+
+    #def verify_active_application_token(self, token):
         #  TODO
-        pass
+     #   pass
 
     ## Retrieve username associated with application_token from platform db
     #
@@ -62,8 +112,16 @@ class DatabaseHandler(object):
     #
     # @return username [string] The username
     def get_token_user(self, app_token):
-        #  TODO
-        pass
+      ros_service = rospy.get_param("rapp_mysql_wrapper_get_username_associated_with_application_token_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_get_username_associated_with_application_token_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, getUsernameAssociatedWithApplicationTokenSrv)
+      req = getUsernameAssociatedWithApplicationTokenSrvRequest()
+      req.application_token=app_token        
+      response = test_service(req)  
+      return response.username   
+
 
     ## Check if there is an active session (token) for the specified user and
     # store_token (device_token)
@@ -73,25 +131,59 @@ class DatabaseHandler(object):
     #
     # @return status [bool] True if token exists, false otherwise
     def verify_active_robot_session(self, username, store_token):
-        #  TODO
-        pass
+      ros_service = rospy.get_param("rapp_mysql_wrapper_check_active_application_token_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_check_active_application_token_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, checkActiveApplicationTokenSrv)
+      req = checkActiveApplicationTokenSrvRequest()
+      req.username=username
+      req.device_token=store_token        
+      response = test_service(req)     
+      if(response.success):
+        return response.application_token_exists
 
     ## Write new user to the platform db
     #
     # @param username [string] The username
     # @param password [string] The user's password
     # @param store_token [string] The user's store token
+    # @param language [string] The user's language
     #
     # @return status [bool] True if token exists, false otherwise
-    def add_new_user(self, username, password, store_token):
-        #  TODO
-        pass
+    def add_new_user(self, username, password, store_token, language):
+      ros_service = rospy.get_param("rapp_mysql_wrapper_create_new_platform_user_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_create_new_platform_user_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, createNewPlatformUserSrv)
+      req = createNewPlatformUserSrvRequest()
+      req.username=username
+      req.password=password
+      req.store_token=store_token
+      req.language=language
+      response = test_service(req)     
+      if(response.success):
+        print "success"
+
 
     ## Write new token to the platform db
     #
     # @param username [string] The user issuing the token
     # @param store_token [string] The device_token of the application token
     # @param app_token [string] The user's application token
+
     def write_new_application_token(self, username, store_token, appl_token):
-        #  TODO
-        pass
+      ros_service = rospy.get_param("rapp_mysql_wrapper_create_new_application_token_service_topic")
+      if(not ros_service):
+        rospy.logerror("rapp_mysql_wrapper_create_new_application_token_service_topic NOT FOUND ERROR")
+      rospy.wait_for_service(ros_service)    
+      test_service = rospy.ServiceProxy(ros_service, createNewApplicationTokenSrv)
+      req = createNewApplicationTokenSrvRequest()
+      req.username=username
+      req.password=password
+      req.store_token=store_token
+      req.language=language
+      response = test_service(req)     
+      if(response.success):
+        print "success"
