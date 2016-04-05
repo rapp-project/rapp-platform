5629c1f0bb3d5316b1d02574a6e848db1d5e0e87
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-04-05 17:27:39 +0300

    New Web service - register_user_from_store

---
+++ rapp_web_services/services/register_user_from_store/iface_obj.js
@@ -0,0 +1,62 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+var clientRes = function(){
+  var obj = {
+    error: '',
+    suggested_username: ''
+  };
+  return obj;
+};
+
+
+var clientReq = function(){
+  var obj = {
+    username: '',
+    password: '',
+    device_token: '',
+    language: ''
+  };
+  return obj;
+};
+
+
+// UserTokenAuthenticationSrv.srv
+var rosReq = function(){
+  var obj = {
+    username: '',
+    password: '',
+    device_token: '',
+    language: ''
+  };
+  return obj;
+};
+
+
+var rosRes = function(){
+  return {error: '', suggested_username: ''};
+
+};
+
+exports.client_res = clientRes;
+exports.client_req = clientReq;
+exports.ros_req = rosReq;
+exports.ros_res = rosRes;
---
+++ rapp_web_services/services/register_user_from_store/svc.js
@@ -0,0 +1,168 @@
+/***
+ * Copyright 2015 RAPP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors: Konstantinos Panayiotou
+ * Contact: klpanagi@gmail.com
+ *
+ */
+
+
+var hop = require('hop');
+var path = require('path');
+var util = require('util');
+
+var PKG_DIR = ENV.PATHS.PKG_DIR;
+var INCLUDE_DIR = ENV.PATHS.INCLUDE_DIR;
+
+var svcUtils = require(path.join(INCLUDE_DIR, 'common',
+    'svc_utils.js'));
+
+var auth = require(path.join(INCLUDE_DIR, 'common',
+    'auth.js'));
+
+var RandStringGen = require ( path.join(INCLUDE_DIR, 'common',
+    'randStringGen.js') );
+
+var ROS = require( path.join(INCLUDE_DIR, 'rosbridge', 'src',
+    'Rosbridge.js') );
+
+var interfaces = require( path.join(__dirname, 'iface_obj.js') );
+
+/* ------------< Load parameters >-------------*/
+var svcParams = ENV.SERVICES.register_user_from_store;
+var rosSrvName = svcParams.ros_srv_name;
+/* ----------------------------------------------------------------------- */
+
+// Initiate communication with rosbridge-websocket-server
+var ros = new ROS({hostname: ENV.ROSBRIDGE.HOSTNAME, port: ENV.ROSBRIDGE.PORT,
+  reconnect: true, onconnection: function(){
+    // .
+  }
+});
+
+
+/*----------------< Random String Generator configurations >---------------*/
+var stringLength = 5;
+var randStrGen = new RandStringGen( stringLength );
+/* ----------------------------------------------------------------------- */
+
+
+function svcImpl( kwargs )
+{
+  //for( var k in this ) {
+    //console.log( k, "=", this[ k ] );
+  //}
+
+  var request = this;
+
+  /***
+   * Asynchronous http response.
+   */
+  return hop.HTTPResponseAsync(
+    function( sendResponse ) {
+      kwargs = kwargs || {};
+      var req = new interfaces.client_req();
+      var response = new interfaces.client_res();
+      var error = '';
+
+
+      /***
+       * Sniff argument values from request body and
+       * create client_req object
+       */
+      try{
+        svcUtils.parseReq(kwargs, req);
+      }
+      catch(e){
+        error = "Service call arguments error";
+        response.error = error;
+        sendResponse( hop.HTTPResponseJson(response) );
+        return;
+      }
+      /* ------------------------------------------ */
+
+      // Assign a unique identification key for this service request.
+      var unqCallId = randStrGen.createUnique();
+
+      var rosMsg = new interfaces.ros_req();
+      rosMsg.username = req.username;
+      rosMsg.password = req.password;
+      rosMsg.device_token = req.device_token;
+      rosMsg.language = req.language;
+
+
+      function callback(data){
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        //console.log(data);
+        // Craft client response using ros service ws response.
+        var response = parseRosbridgeMsg( data );
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+      }
+
+      function onerror(e){
+        // Remove this call id from random string generator cache.
+        randStrGen.removeCached( unqCallId );
+        // craft error response
+        var response = new interfaces.client_res();
+        response.error = svcUtils.ERROR_MSG_DEFAULT;
+        // Asynchronous response to client.
+        sendResponse( hop.HTTPResponseJson(response) );
+      }
+
+
+      ros.callService(rosSrvName, rosMsg,
+        {success: callback, fail: onerror});
+
+      /***
+       *  Timeout this request. Return to client.
+       */
+      setTimeout(function(){
+        var response = new interfaces.client_res();
+        response.error = svcUtils.ERROR_MSG_DEFAULT;
+        sendResponse( hop.HTTPResponseJson(response) );
+      }, svcParams.timeout);
+      /* ----------------------------------------------- */
+
+    }, this );
+}
+
+
+
+
+/***
+ * Craft response object.
+ *
+ *  @param {Object} rosbridge_msg - Return message from rosbridge
+ *  @returns {Object} response - Response Object.
+ *
+ */
+function parseRosbridgeMsg(rosbridge_msg)
+{
+  var response = new interfaces.client_res();
+  var error = rosbridge_msg.error;
+
+  if( error ){
+    response.error = error;
+    return response;
+  }
+
+  response.suggested_username = rosbridge_msg.suggested_username;
+  return response;
+}
+
+
+registerSvc(svcImpl, svcParams);
