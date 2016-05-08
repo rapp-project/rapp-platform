c938ff279bc419e35847a3b98214063b87a2bb52
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-05-09 00:47:35 +0300

    Extended req-parser for files.

--- rapp_web_services/src/webService/fileParser.js
+++ rapp_web_services/src/webService/fileParser.js
@@ -28,36 +28,64 @@
  *  @copyright Rapp Project EU 2016
  */
 
+var fs = require('fs');
 
-function FileParser(fields) {
-  this.fields = fields || ['file'];
+
+function FileParser() {
 }
 
 
-FileParser.prototype.parse = function(req) {
-  var files = [];
+FileParser.prototype.isFile = function(path){
+  if(! (typeof path === 'string' || path instanceof String) ){
+    return false;
+  }
+  var _isfile = false;
+  if( fs.existsSync(path) ) {_isfile = fs.lstatSync(path).isFile();}
+  return _isfile;
+}
 
-  for (var i in this.fields){
-    if(! req.body[this.fields[i]]){
-      continue;
+
+FileParser.prototype.isArrayOfFiles = function(array){
+  if(array instanceof Array && array.length){
+    for(var i in array){
+      if(! this.isFile(array[i])){
+        return false
+      }
     }
-    else if (req.body[this.fields[i]] instanceof Array) {
-      //An Array of files
-      files.concat(req.body[thisfields[i]]);
-      // Remove from req.body
-      delete req.body[this.fields[i]];
+    return true;
+  }
+  return false;
+}
+
+
+/*!
+ * @brief Parses the request arguments for files. Adds any received files
+ * in req.files. For example "imagefile" can be retrieved from:
+ * req.files.imagefile[0]
+ * It is an array because multiple files can be received from
+ * multipart/form-data requests as an Array of files, having the same form name.
+ *
+ * @param {Object} req - The request object.
+ */
+FileParser.prototype.call = function(req) {
+  var _files = {};
+  var removeFromBody = [];
+
+  for (var i in req.body){
+    if (this.isArrayOfFiles(req.body[i])) {
+      _files[i] = req.body[i];
+      delete req.body[i];
     }
-    else{
+    else if(this.isFile(req.body[i])){
       // Meaning single file.
-      files.push(req.body[this.fields[i]]);
-      // Remove from req.body
-      delete req.body[this.fields[i]];
+      _files[i] = [req.body[i]];
+      delete req.body[i];
     }
   }
   // Add transfered file entries in req.files (Array).
-  req.files = files;
+  req.files = _files;
 };
 
-module.exports = function(fields) {
-  return new FileParser(fields);
+module.exports = function() {
+  return new FileParser();
 };
