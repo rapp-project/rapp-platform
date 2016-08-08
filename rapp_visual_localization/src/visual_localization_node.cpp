525192a11d4ea83e9595da7c57fe97e43f8e82cd
maciek-slon <maciek.slon@gmail.com>
2016-08-08 14:07:30 +0200

    Add node

---
+++ rapp_visual_localization/src/visual_localization_node.cpp
@@ -0,0 +1,181 @@
+#include "ros/ros.h"
+
+#include "rapp_visual_localization/VisOdom.hpp"
+
+class VisualLocalizationNode {
+public:
+  void createMap() {
+    method = M_COLORMAX; // typ zestawu cech obrazu
+    int numImgs=3; // liczba obrazów sylwetki
+    // Obiekt z parametrami definiowanymi przez użytkownika
+    upa = CUserParameters(numImgs, numImgs); 
+    // liczba obrazów sylwetki i tworzonych modeli sylwetki 
+    
+    // Krok 3: Utworzenie wizualnej mapy pomieszczenia
+    int numx=19; // liczba kolumn w mapie
+    int numz=41; // liczba rzędów w mapie
+    int numd=4; // liczba kierunków
+    int vgrid = 5; // (nieistotne)
+
+    objVO.initMap(numx, numz, numd, vgrid); // inicjalizuj dane związane z rozmiarem mapy
+    objVO.initFeatureMemory(method); // alokuj pamięć dla reprezentacji cech obrazów (w fazie tworzenia mapy)
+    objVO.initFeatureScaling(method); // inicjalizuj wagi cech (potrzebne zarówno w fazie uczenia jak i testowania/pracy)
+    
+    
+    MapXml = "work7ModelColorMax.xml"; // plik do zapisu tworzonej wizualnej mapy pomieszczenia
+    MeasXml = "work7MeasureColorMax.xml"; // plik z przygotowaną mapą obserwacji (tryb off-line) 
+    
+    // Utworzenie mapy i wypisanie jej do pliku xml
+    objVO.createMap(method, MapXml.c_str(), MeasXml.c_str(), 0, 0); // parameter 1 określa typ zestawu cech obrazu
+    // parametr 2: nazwa pliku z istniejącą mapą
+    // parametr 3: nazwa pliku z pomiarami dla wszystkich stanów (w trybie testowania/symulacji)
+    // parametr 4: tworzenie mapy (1) lub pomiń to (0) bo plik xml z mapą już istnieje (0)
+    // parametr 5: na potrzeby testowania/symulacji utwórz wcześniej mapę cech dla wszystkich aktualnych pomiarów (1) lub pomiń (0)  
+
+
+    char *inH = &inDirHuman[0];
+    char *ouH = &ouDirHuman[0];
+    char HumanXml[] = "humanModel.xml"; // plik z modelem sylwetki
+
+
+    // Krok 4: Utworzenie modelu aktualnej sylwetki
+    objH.createModel(inH, ouH, HumanXml, 0, VIS, MONIT, LOG);
+    // parametr 3: =0 : wczytaj model sylwetki z domyslnego pliku; =1: utwórz na nowo model sylwetki
+  }
+  
+  void doWork() {
+    int MAXITER = 100; // dopuszczalna maksymalna liczba kroków jednego procesu nawigacji
+    int TESTNUM = 100; // zadana liczba testów nawigacji
+
+    ::cv::Mat results;
+
+    // Metod "work" może być podstawą wątku ale nie jest to konieczne
+    // W trybie "test/symulacja": TESTNUM oznacza liczbę testów (ścieżek)
+    // W trybie "praca on-line": TESTNUM będzie liczbą ostatnich pamiętanych wyników
+    results = objVO.work(TESTNUM, MAXITER, method, objH, MapXml.c_str(), MeasXml.c_str(), false);
+    // ostatni parametr: true = tryb off-line (testowanie w warunkach symulacji pomiarów)
+    // false = tryb on-line (rzeczywista nawigacja z samolokalizacją)
+    
+    std::string MapXml;
+    std::string MeasXml;
+  }
+
+private:
+  // Obiekty potrzebne do analizy obrazów
+  CVisOdom objVO; // obiekt dla wizualnej odometrii
+  CHuman objH;    // obiekt dla modelowania i detekcji sylwetki
+  
+  MType method;   // typ zestawu cech obrazu
+  // Obiekt z parametrami definiowanymi przez użytkownika
+  CUserParameters	upa = CUserParameters(numImgs, numImgs); 
+  // liczba obrazów sylwetki i tworzonych modeli sylwetki 
+};
+
+/**
+ * Serves the object detection ROS service callback
+ * 
+ * \param req [in]  The ROS service request
+ * \param res [out] The ROS service response
+ * 
+ * \return The success status of the call
+ */
+/*bool service_FindObjects(rapp_platform_ros_communications::FindObjectsSrv::Request  &req,
+                         rapp_platform_ros_communications::FindObjectsSrv::Response &res)
+{
+  res.result = detectors[req.user].findObjects(req.user, req.fname, req.limit, res.found_names, res.found_centers, res.found_scores);
+  return true;
+}*/
+
+/**
+ * Serves the object learning ROS service callback
+ * 
+ * \param req [in]  The ROS service request
+ * \param res [out] The ROS service response
+ * 
+ * \return The success status of the call
+ */
+/*bool service_LearnObject(rapp_platform_ros_communications::LearnObjectSrv::Request  &req,
+                         rapp_platform_ros_communications::LearnObjectSrv::Response &res)
+{
+  res.result = detectors[req.user].learnObject(req.user, req.fname, req.name);
+  return true;
+}*/
+
+/**
+ * Serves the models loading ROS service callback
+ * 
+ * \param req [in]  The ROS service request
+ * \param res [out] The ROS service response
+ * 
+ * \return The success status of the call
+ */
+/*bool service_LoadModels(rapp_platform_ros_communications::LoadModelsSrv::Request  &req,
+                         rapp_platform_ros_communications::LoadModelsSrv::Response &res)
+{
+  detectors[req.user].loadModels(req.user, req.names, res.result);
+  return true;
+}*/
+
+/**
+ * Serves the models clearing ROS service callback
+ * 
+ * \param req [in]  The ROS service request
+ * \param res [out] The ROS service response
+ * 
+ * \return The success status of the call
+ */
+/*bool service_ClearModels(rapp_platform_ros_communications::ClearModelsSrv::Request  &req,
+                         rapp_platform_ros_communications::ClearModelsSrv::Response &res)
+{
+  detectors[req.user].clearModels(req.user);
+  return true;
+}*/
+
+/**
+ * The executable's main function.
+ * Creates a ROS multispinner to enable concurrent requests and creates services
+ */
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "find_objects_node");
+  ros::NodeHandle n;
+
+  std::string service_name;
+  
+  VisualLocalizationNode vl_node;
+  vl_node.createMap();
+  vl_node.doWork();
+/*  if (!n.getParam("/rapp_object_recognition_topic", service_name))
+    ROS_ERROR("rapp_object_recogntion_topic not set!");
+  ros::ServiceServer service = n.advertiseService(service_name, service_FindObjects);
+  
+  if (!n.getParam("/rapp_object_learn_topic", service_name))
+    ROS_ERROR("rapp_object_learn_topic not set!");
+  ros::ServiceServer service2 = n.advertiseService(service_name, service_LearnObject);
+  
+  if (!n.getParam("/rapp_object_load_topic", service_name))
+    ROS_ERROR("rapp_object_load_topic not set!");
+  ros::ServiceServer service3 = n.advertiseService(service_name, service_LoadModels);
+  
+  if (!n.getParam("/rapp_object_clear_topic", service_name))
+    ROS_ERROR("rapp_object_clear_topic not set!");
+  ros::ServiceServer service4 = n.advertiseService(service_name, service_ClearModels);
+  
+  ROS_INFO("Ready to find objects.");*/
+  
+  int threads = 1;
+  /*if(!n.getParam("/rapp_object_recognition_threads", threads))
+  {
+    ROS_WARN("Hazard detection threads param not found");
+  }
+  else if(threads < 0)
+  {
+    threads = 1;
+  }*/
+  
+  ROS_INFO("Ready to recognize!");
+  ros::MultiThreadedSpinner spinner(threads);
+  spinner.spin();
+  
+  return 0;
+}
