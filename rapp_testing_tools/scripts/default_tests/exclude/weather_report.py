f05852ae46527c35a705f9f63b45bf9d11a744d4
Konstantinos Panayiotou <klpanagi@gmail.com>
2016-11-08 18:13:45 +0200

    Few weather-reported and news-explorer tests

--- rapp_testing_tools/scripts/default_tests/exclude/weather_report.py
+++ rapp_testing_tools/scripts/default_tests/exclude/weather_report.py
@@ -18,28 +18,42 @@
 # Authors: Konstantinos Panayiotou, Manos Tsardoulias
 # contact: klpanagi@gmail.com, etsardou@iti.gr
 
+
 import os
+import timeit
 import unittest
-import rospkg
 
 __path__ = os.path.dirname(os.path.realpath(__file__))
 
 from RappCloud import RappPlatformAPI
 
 
-class NewsExploreTests(unittest.TestCase):
+class WeatherReportTests(unittest.TestCase):
 
     def setUp(self):
         self.ch = RappPlatformAPI()
 
-    def test_default(self):
-        response = self.ch.newsExplore(news_engine='',
-                                       keywords=['football'],
-                                       exclude_titles=[],
-                                       region='',
-                                       topic='',
-                                       num_news=2)
-        print response
+    def test_current_with_city(self):
+        response = self.ch.weatherReportCurrent('Athens', '', 0)
+        valid_resp_struct = {
+            'date': '', 'temperature': '', 'weather_description': '', 'humidity': '',
+            'visibility': '', 'pressure': '', 'wind_speed': '', 'wind_temperature': '',
+            'wind_direction': '', 'error': ''
+        }
+        self.assertDictKeysEqual(response, valid_resp_struct)
+
+    def test_forecast_with_city(self):
+        response = self.ch.weatherReportForecast('Athens', '', 0)
+        valid_resp_struct = {
+            'forecast': [], 'error': ''
+        }
+        self.assertDictKeysEqual(response, valid_resp_struct)
+
+    def assertDictKeysEqual(self, d1, d2, msg=None):
+        for key, val in d1.iteritems():
+            self.assertIn(key, d2, msg)
+            
+
 
 
 if __name__ == "__main__":
